9f57251d84819e1199388fb82db56920898f42de
Harry Liu <byliuyang11@gmail.com>
2019-07-13 23:39:09 -0700

    Add time scalar

---
+++ app/graphql/scalar/time.go
@@ -0,0 +1,52 @@
+package scalar
+
+import (
+	"encoding/json"
+	"time"
+	"tinyURL/app/graphql/parser"
+	"tinyURL/fw"
+
+	"github.com/pkg/errors"
+)
+
+type Time struct {
+	time.Time
+}
+
+func (Time) ImplementsGraphQLType(name string) bool {
+	return name == "Time"
+}
+
+func (t *Time) UnmarshalGraphQL(input interface{}) error {
+	var timeTmp time.Time
+	var err error
+
+	switch input := input.(type) {
+	case time.Time:
+		timeTmp = input
+		err = nil
+	case string:
+		timeTmp, err = parser.NewStringTime().FromVal(input)
+	case int:
+		timeTmp, err = parser.NewIntTime().FromVal(input)
+	case float64:
+		timeTmp, err = parser.NewFloatTime().FromVal(input)
+	default:
+		err = errors.New("wrong type")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	t.Time = timeTmp
+	return nil
+}
+
+func (t Time) MarshalJSON() ([]byte, error) {
+	return json.Marshal(t.Time)
+}
+
+func NewTime() fw.Scalar {
+	return &Time{}
+}
