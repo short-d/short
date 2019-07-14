459c20ad48e25b502aedebb57730b9b20ba7544e
Harry Liu <byliuyang11@gmail.com>
2019-07-13 23:51:33 -0700

    Add Scalar interface

--- fw/graphql.go
+++ fw/graphql.go
@@ -6,3 +6,9 @@ type GraphQlApi interface {
 	GetSchema() string
 	GetResolver() Resolver
 }
+
+type Scalar interface {
+	ImplementsGraphQLType(name string) bool
+	UnmarshalGraphQL(input interface{}) error
+	MarshalJSON() ([]byte, error)
+}
