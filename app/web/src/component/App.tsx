a64defba7a04ca602f26a2993c10e91e140d40f9
Harry Liu <byliuyang11@gmail.com>
2019-08-04 14:53:20 -0700

    Add Footer to home page

--- app/web/src/component/App.tsx
+++ app/web/src/component/App.tsx
@@ -6,6 +6,7 @@ import {TextField} from './form/TextField';
 import {Button} from './Button';
 import {Url} from '../entity/Url';
 import {UrlService} from '../service/url.service';
+import {Footer} from './Footer';
 
 interface Props {
 }
@@ -59,28 +60,32 @@ export class App extends Component<Props, State> {
         return (
             <div className='app'>
                 <Header/>
-                <Section title={'New Short Link'}>
-                    <div className={'control create-short-link'}>
-                        <div className={'text-field-wrapper'}>
-                            <TextField text={this.state.editingUrl.originalUrl} placeHolder={'Long Link'}
-                                       onChange={this.handlerOriginalUrlChange}/>
+                <div className={'main'}>
+                    <Section title={'New Short Link'}>
+                        <div className={'control create-short-link'}>
+                            <div className={'text-field-wrapper'}>
+                                <TextField text={this.state.editingUrl.originalUrl} placeHolder={'Long Link'}
+                                           onChange={this.handlerOriginalUrlChange}/>
+                            </div>
+                            <div className={'text-field-wrapper'}>
+                                <TextField text={this.state.editingUrl.alias}
+                                           placeHolder={'Custom Short Link ( Optional )'}
+                                           onChange={this.handleAliasChange}/>
+                            </div>
+                            <Button onClick={this.handleCreateShortLinkClick}>Create Short Link</Button>
                         </div>
-                        <div className={'text-field-wrapper'}>
-                            <TextField text={this.state.editingUrl.alias} placeHolder={'Custom Short Link ( Optional )'}
-                                       onChange={this.handleAliasChange}/>
-                        </div>
-                        <Button onClick={this.handleCreateShortLinkClick}>Create Short Link</Button>
-                    </div>
-                    {this.state.createdUrl ?
-                        <div className={'short-link-notice'}>
-                            You can now paste&nbsp;
-                            <a target={'_blank'}
-                               href={`${this.state.baseUrl}${this.state.createdUrl.alias}`}>{`${this.state.baseUrl}${this.state.createdUrl.alias}`}</a>
-                            &nbsp;in your browser to visit {this.state.createdUrl.originalUrl}.
-                        </div> :
-                        false
-                    }
-                </Section>
+                        {this.state.createdUrl ?
+                            <div className={'short-link-notice'}>
+                                You can now paste&nbsp;
+                                <a target={'_blank'}
+                                   href={`${this.state.baseUrl}${this.state.createdUrl.alias}`}>{`${this.state.baseUrl}${this.state.createdUrl.alias}`}</a>
+                                &nbsp;in your browser to visit {this.state.createdUrl.originalUrl}.
+                            </div> :
+                            false
+                        }
+                    </Section>
+                </div>
+                <Footer authorName={'Harry'} authorPortfolio={'https://github.com/byliuyang'}/>
             </div>
         );
     };
