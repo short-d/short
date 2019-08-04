1915529efbaa73c87c2246b52b8222c5527c3e8d
Harry Liu <byliuyang11@gmail.com>
2019-08-04 01:39:06 -0700

    Short link notice (#23)
    * Rename /api/redirect ro /r
    
    * Add short link notice

--- app/web/src/component/App.tsx
+++ app/web/src/component/App.tsx
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import './App.scss';
-import {Header} from "./Header";
-import {Section} from "./Section";
-import {TextField} from "./form/TextField";
-import {Button} from "./Button";
-import {Url} from "../entity/Url";
-import {UrlService} from "../service/url.service";
+import {Header} from './Header';
+import {Section} from './Section';
+import {TextField} from './form/TextField';
+import {Button} from './Button';
+import {Url} from '../entity/Url';
+import {UrlService} from '../service/url.service';
 
 interface Props {
 }
 
 interface State {
-    url: Url
+    editingUrl: Url
+    createdUrl?: Url
+    baseUrl: string
 }
 
 export class App extends Component<Props, State> {
@@ -20,16 +22,17 @@ export class App extends Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            url: {
-                originalUrl: "",
-                alias: ""
-            }
+            editingUrl: {
+                originalUrl: '',
+                alias: ''
+            },
+            baseUrl: 'http://localhost/r/'
         };
     }
 
     handlerOriginalUrlChange = (newValue: string) => {
         this.setState({
-            url: Object.assign({}, this.state.url, {
+            editingUrl: Object.assign({}, this.state.editingUrl, {
                 originalUrl: newValue
             })
         });
@@ -38,32 +41,45 @@ export class App extends Component<Props, State> {
 
     handleAliasChange = (newValue: string) => {
         this.setState({
-            url: Object.assign({}, this.state.url, {
+            editingUrl: Object.assign({}, this.state.editingUrl, {
                 alias: newValue
             })
         });
     };
 
     handleCreateShortLinkClick = () => {
-        this.urlService.createShortLink(this.state.url)
+        this.urlService
+            .createShortLink(this.state.editingUrl)
+            .then((url: Url) => this.setState({
+                createdUrl: url
+            }));
     };
 
     render = () => {
         return (
-            <div className='App'>
+            <div className='app'>
                 <Header/>
                 <Section title={'New Short Link'}>
                     <div className={'control create-short-link'}>
                         <div className={'text-field-wrapper'}>
-                            <TextField text={this.state.url.originalUrl} placeHolder={'Long Link'}
+                            <TextField text={this.state.editingUrl.originalUrl} placeHolder={'Long Link'}
                                        onChange={this.handlerOriginalUrlChange}/>
                         </div>
                         <div className={'text-field-wrapper'}>
-                            <TextField text={this.state.url.alias} placeHolder={'Custom Short Link ( Optional )'}
+                            <TextField text={this.state.editingUrl.alias} placeHolder={'Custom Short Link ( Optional )'}
                                        onChange={this.handleAliasChange}/>
                         </div>
                         <Button onClick={this.handleCreateShortLinkClick}>Create Short Link</Button>
                     </div>
+                    {this.state.createdUrl ?
+                        <div className={'short-link-notice'}>
+                            You can now paste&nbsp;
+                            <a target={'_blank'}
+                               href={`${this.state.baseUrl}${this.state.createdUrl.alias}`}>{`${this.state.baseUrl}${this.state.createdUrl.alias}`}</a>
+                            &nbsp;in your browser to visit {this.state.createdUrl.originalUrl}.
+                        </div> :
+                        false
+                    }
                 </Section>
             </div>
         );
