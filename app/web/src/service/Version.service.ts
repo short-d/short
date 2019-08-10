fc3d79b622e92a30d50b06bf5ad2cfac2676c159
Harry Liu <byliuyang11@gmail.com>
2019-08-10 13:56:55 -0700

    Format code

--- app/web/src/service/Version.service.ts
+++ app/web/src/service/Version.service.ts
@@ -1,7 +1,7 @@
 import {EnvService} from './Env.service';
 
 export class VersionService {
-    static getAppVersion():string {
+    static getAppVersion(): string {
         return EnvService.getVal('GIT_SHA');
     }
 
