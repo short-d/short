8d4c48c738ecbdc4611ea32db6ba8a1471b6d6a3
Harry Liu <byliuyang11@gmail.com>
2019-08-10 13:55:03 -0700

    Display AliasAlreadyExist to user.

--- app/web/src/service/Url.service.tsx
+++ app/web/src/service/Url.service.tsx
@@ -1,21 +1,30 @@
 import {Url} from '../entity/Url';
 import {ApolloClient} from 'apollo-client';
-import {createHttpLink} from 'apollo-link-http';
+import {HttpLink} from 'apollo-link-http';
 import {InMemoryCache} from 'apollo-cache-inmemory';
-import {FetchResult} from 'apollo-link';
+import {ApolloLink, FetchResult} from 'apollo-link';
 import gql from 'graphql-tag';
 import {EnvService} from './Env.service';
+import {GraphQlError} from '../graphql/error';
+
+const gqlLink = ApolloLink.from([
+    new HttpLink({
+        uri: `${EnvService.getVal('GRAPHQL_API_BASE_URL')}/graphql`
+    })
+]);
+
+const gqlClient = new ApolloClient(
+    {
+        link: gqlLink,
+        cache: new InMemoryCache(),
+    }
+);
+
+export enum ErrUrl {
+    AliasAlreadyExist = 'aliasAlreadyExist'
+}
 
 export class UrlService {
-    private gqlClient = new ApolloClient(
-        {
-            link: createHttpLink({
-                uri: `${EnvService.getVal('GRAPHQL_API_BASE_URL')}/graphql`
-            }),
-            cache: new InMemoryCache()
-        }
-    );
-
     createShortLink(link: Url): Promise<Url> {
         let alias = link.alias === '' ? null : link.alias;
 
@@ -35,11 +44,17 @@ export class UrlService {
             }
         `;
 
-        return this.gqlClient.mutate({
-            variables: variables,
-            mutation: mutation,
-        })
-            .then((res: FetchResult<Url>) => res.data.createUrl);
+        return new Promise<Url>(((resolve, reject: (errCodes: ErrUrl[]) => any) => {
+            gqlClient.mutate({
+                variables: variables,
+                mutation: mutation,
+            })
+                .then((res: FetchResult<Url>) => resolve(res.data.createUrl))
+                .catch(({graphQLErrors, networkError, message}) => {
+                    const errCodes = graphQLErrors.map((graphQLError: GraphQlError) => graphQLError.extensions.code);
+                    reject(errCodes);
+                });
+        }));
     }
 
     aliasToLink(alias: string): string {
