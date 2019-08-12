de756f1decc24983b26169815d53aaddd3ab3073
Harry Liu <byliuyang11@gmail.com>
2019-08-11 23:53:42 -0700

    Integrate reCAPTCHA with Web UI (#69)
    * Integrate reCAPTCHA with Web UI
    
    * Fix Dockerfile-test-web and add drone REACT_APP_RECAPTCHA_SITE_KEY environment variable to web ui build & test
    
    * Rename REACT_APP_RECAPTCHA_SITE_KEY to RECAPTCHA_SITE_KEY

--- app/adapter/web/src/index.tsx
+++ app/adapter/web/src/index.tsx
@@ -4,9 +4,16 @@ import './index.scss';
 import App from './component/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import {CaptchaService} from "./service/Captcha.service";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+CaptchaService
+    .InitRecaptchaV3()
+    .then(reCaptcha => {
+        ReactDOM.render(
+            <App reCaptcha={reCaptcha}/>,
+            document.getElementById('root'));
+        // If you want your app to work offline and load faster, you can change
+        // unregister() to register() below. Note this comes with some pitfalls.
+        // Learn more about service workers: https://bit.ly/CRA-PWA
+        serviceWorker.unregister();
+    });
