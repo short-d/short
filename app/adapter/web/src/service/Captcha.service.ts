fe6af5f5d9c520a6355e9075d6d16783162c9212
Harry Liu <byliuyang11@gmail.com>
2019-08-13 00:05:55 -0700

    Rename .service.tsx files to .service.ts

--- app/adapter/web/src/service/Captcha.service.ts
+++ app/adapter/web/src/service/Captcha.service.ts
@@ -2,11 +2,11 @@ import {load} from "recaptcha-v3";
 import {EnvService} from "./Env.service";
 
 export interface ReCaptcha {
-    execute: (action: string)=> Promise<string>
+    execute: (action: string) => Promise<string>
 }
 
 export class CaptchaService {
-    static InitRecaptchaV3(): Promise<ReCaptcha>{
+    static InitRecaptchaV3(): Promise<ReCaptcha> {
         return load(EnvService.getVal('RECAPTCHA_SITE_KEY'));
     }
 }
