a2bff8f76073de81086427c4ea8133a3d26c484a
Harry Liu <byliuyang11@gmail.com>
2019-08-13 08:32:19 -0700

    Add long link and custom alias format validation (#79)

---
+++ app/adapter/graphql/resolver/error.go
@@ -0,0 +1,80 @@
+package resolver
+
+type ErrCode string
+
+const (
+	ErrCodeUnknown            ErrCode = "unknown"
+	ErrCodeAliasAlreadyExist          = "aliasAlreadyExist"
+	ErrCodeRequesterNotHuman          = "requestNotHuman"
+	ErrCodeInvalidLongLink            = "invalidLongLink"
+	ErrCodeInvalidCustomAlias         = "invalidCustomAlias"
+)
+
+type ErrUnknown struct{}
+
+func (e ErrUnknown) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code": ErrCodeUnknown,
+	}
+}
+
+func (e ErrUnknown) Error() string {
+	return "unknown err"
+}
+
+type ErrUrlAliasExist string
+
+func (e ErrUrlAliasExist) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code":  ErrCodeAliasAlreadyExist,
+		"alias": string(e),
+	}
+}
+
+func (e ErrUrlAliasExist) Error() string {
+	return "url alias already exists"
+}
+
+type ErrNotHuman struct{}
+
+func (e ErrNotHuman) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code": ErrCodeRequesterNotHuman,
+	}
+}
+
+func (e ErrNotHuman) Error() string {
+	return "requester is not human"
+}
+
+type ErrInvalidLongLink string
+
+func (e ErrInvalidLongLink) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code":     ErrCodeInvalidLongLink,
+		"longLink": string(e),
+	}
+}
+
+func (e ErrInvalidLongLink) Error() string {
+	return "long link is invalid"
+}
+
+type ErrInvalidCustomAlias struct {
+	customAlias *string
+}
+
+func (e ErrInvalidCustomAlias) Extensions() map[string]interface{} {
+	var alias string
+	if e.customAlias != nil {
+		alias = *e.customAlias
+	}
+	return map[string]interface{}{
+		"code":        ErrCodeInvalidCustomAlias,
+		"customAlias": alias,
+	}
+}
+
+func (e ErrInvalidCustomAlias) Error() string {
+	return "custom alias is invalid"
+}
--- app/adapter/graphql/resolver/mutation.go
+++ app/adapter/graphql/resolver/mutation.go
@@ -4,16 +4,18 @@ import (
 	"short/app/entity"
 	"short/app/usecase/captcha"
 	"short/app/usecase/url"
+	"short/app/usecase/validator"
 	"short/fw"
-	"strings"
 	"time"
 )
 
 type Mutation struct {
-	logger          fw.Logger
-	tracer          fw.Tracer
-	urlCreator      url.Creator
-	captchaVerifier captcha.Verifier
+	logger            fw.Logger
+	tracer            fw.Tracer
+	urlCreator        url.Creator
+	captchaVerifier   captcha.Verifier
+	longLinkValidator validator.Validator
+	aliasValidator    validator.Validator
 }
 
 type UrlInput struct {
@@ -26,85 +28,6 @@ type CreateUrlArgs struct {
 	CaptchaResponse string
 	Url             UrlInput
 	UserEmail       *string
-}
-
-type ErrCode string
-
-const (
-	ErrCodeUnknown             ErrCode = "unknown"
-	ErrCodeAliasAlreadyExist           = "aliasAlreadyExist"
-	ErrCodeOriginalUrlTooShort         = "originalUrlTooShort"
-	ErrCodeWrongUriFormat              = "wrongUriFormat"
-	ErrCodeRequesterNotHuman           = "requestNotHuman"
-)
-
-const minUrlLength = 6
-
-type ErrUnknown struct{}
-
-func (e ErrUnknown) Extensions() map[string]interface{} {
-	return map[string]interface{}{
-		"code": ErrCodeUnknown,
-	}
-}
-
-func (e ErrUnknown) Error() string {
-	return "unknown err"
-}
-
-type ErrUrlAliasExist string
-
-func (e ErrUrlAliasExist) Extensions() map[string]interface{} {
-	return map[string]interface{}{
-		"code":  ErrCodeAliasAlreadyExist,
-		"alias": string(e),
-	}
-}
-
-func (e ErrUrlAliasExist) Error() string {
-	return "url alias already exists"
-}
-
-type ErrOriginalUrlTooShort string
-
-func (e ErrOriginalUrlTooShort) Extensions() map[string]interface{} {
-	return map[string]interface{}{
-		"code":        ErrCodeOriginalUrlTooShort,
-		"originalUrl": string(e),
-	}
-}
-
-func (e ErrOriginalUrlTooShort) Error() string {
-	return "original url is too short"
-}
-
-type ErrWrongUriFormat string
-
-func (e ErrWrongUriFormat) Extensions() map[string]interface{} {
-	return map[string]interface{}{
-		"code":        ErrCodeWrongUriFormat,
-		"originalUrl": string(e),
-	}
-}
-
-func (e ErrWrongUriFormat) Error() string {
-	return "url format is incorrect"
-}
-
-type ErrNotHuman struct{}
-
-func (e ErrNotHuman) Extensions() map[string]interface{} {
-	return map[string]interface{}{
-		"code": ErrCodeRequesterNotHuman,
-	}
-}
-
-func (e ErrNotHuman) Error() string {
-	return "requester is not human"
-}
-
-func isUri(text string) bool {
-	return strings.Contains(text, "://")
 }
 
 func (m Mutation) CreateUrl(args *CreateUrlArgs) (*Url, error) {
@@ -120,13 +43,16 @@ func (m Mutation) CreateUrl(args *CreateUrlArgs) (*Url, error) {
 		return nil, ErrNotHuman{}
 	}
 
-	originalUrl := args.Url.OriginalUrl
-	if len(originalUrl) < minUrlLength {
-		return nil, ErrOriginalUrlTooShort(originalUrl)
+	longLink := args.Url.OriginalUrl
+	if !m.longLinkValidator.IsValid(&longLink) {
+		return nil, ErrInvalidLongLink(longLink)
 	}
 
-	if !isUri(originalUrl) {
-		return nil, ErrWrongUriFormat(originalUrl)
+	customAlias := args.Url.CustomAlias
+	if !m.aliasValidator.IsValid(customAlias) {
+		return nil, ErrInvalidCustomAlias{
+			customAlias: customAlias,
+		}
 	}
 
 	u := entity.Url{
@@ -174,9 +100,11 @@ func NewMutation(
 	captchaVerifier captcha.Verifier,
 ) Mutation {
 	return Mutation{
-		logger:          logger,
-		tracer:          tracer,
-		urlCreator:      urlCreator,
-		captchaVerifier: captchaVerifier,
+		logger:            logger,
+		tracer:            tracer,
+		urlCreator:        urlCreator,
+		captchaVerifier:   captchaVerifier,
+		longLinkValidator: validator.NewLongLink(),
+		aliasValidator:    validator.CustomAlias{},
 	}
 }
