b59f29ec271b09053a3f8498858a59436c30d2af
Harry Liu <byliuyang11@gmail.com>
2019-08-22 00:42:26 -0700

    Fix SQL column misspell

--- app/adapter/migration/2_create_user_table.sql
+++ app/adapter/migration/2_create_user_table.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "email" character varying(254) PRIMARY KEY,
     "name" character varying(80),
-    "lastSignInAt" time with time zone,
+    "lastSignedInAt" time with time zone,
     "createdAt" time with time zone,
     "expireAt" time with time zone,
     "updatedAt" time with time zone
