fc4c92b840ebdace469a6ff155470c8c73376642
Harry Liu <byliuyang11@gmail.com>
2019-08-22 00:03:29 -0700

    Add repo account service

---
+++ app/usecase/account/repo.go
@@ -0,0 +1,36 @@
+package account
+
+import (
+	"short/app/entity"
+	"short/app/usecase/repo"
+	"short/app/usecase/service"
+	"short/fw"
+)
+
+var _ service.Account = (*Repo)(nil)
+
+type Repo struct {
+	userRepo repo.User
+	timer    fw.Timer
+}
+
+func (r Repo) IsAccountExist(email string) (bool, error) {
+	return r.userRepo.IsEmailExist(email)
+}
+
+func (r Repo) CreateAccount(email string, name string) error {
+	now := r.timer.Now()
+	user := entity.User{
+		Email:     email,
+		Name:      name,
+		CreatedAt: &now,
+	}
+	return r.userRepo.Create(user)
+}
+
+func NewRepoService(userRepo repo.User, timer fw.Timer) Repo {
+	return Repo{
+		userRepo: userRepo,
+		timer:    timer,
+	}
+}
