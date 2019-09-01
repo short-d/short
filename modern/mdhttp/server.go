8dddf659d6e49b497ecba944c6c3f07575c4b3e3
Harry Liu <byliuyang11@gmail.com>
2019-09-01 02:23:54 -0700

    Graphql playground (#158)
    * Add GraphQL playground
    
    * Remove random character and scroll bars

--- modern/mdhttp/server.go
+++ modern/mdhttp/server.go
@@ -52,7 +52,7 @@ func setupPreFlight(w http.ResponseWriter) http.ResponseWriter {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers",
-		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, x-apollo-tracing")
 	return w
 }
 
