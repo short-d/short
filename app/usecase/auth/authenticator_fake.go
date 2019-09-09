8937f35d3cb63af6cedfd7c40a5ae88debc7fb6f
Harry Liu <byliuyang11@gmail.com>
2019-09-08 19:50:47 -0700

    Fix tests

--- app/usecase/auth/authenticator_fake.go
+++ app/usecase/auth/authenticator_fake.go
@@ -7,7 +7,7 @@ import (
 )
 
 func NewAuthenticatorFake(current time.Time, validPeriod time.Duration) Authenticator {
-	tokenizer := mdtest.CryptoTokenizerFake{}
+	tokenizer := mdtest.NewCryptoTokenizerFake()
 	timer := mdtest.NewTimerFake(current)
 	return NewAuthenticator(tokenizer, timer, validPeriod)
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module short
 go 1.13
 
 require (
-	github.com/byliuyang/app v0.0.0-20190909012718-3fd71eb5a04b
+	github.com/byliuyang/app v0.0.0-20190909024200-06bca33060d2
 	github.com/google/wire v0.3.0
 	github.com/satori/go.uuid v1.2.0
 )
