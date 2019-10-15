9ffcc70d6974f5678d56de1a1bc3215595fd5d85
Harry Liu <byliuyang11@gmail.com>
2019-10-15 00:10:53 -0700

    Format code

--- frontend/src/component/pages/shared/sign-in/SignInModel.tsx
+++ frontend/src/component/pages/shared/sign-in/SignInModel.tsx
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import './SignInModel.scss';
-import {Modal} from '../../../ui/Modal';
-import {GithubSignInButton} from './GithubSignInButton';
+import { Modal } from '../../../ui/Modal';
+import { GithubSignInButton } from './GithubSignInButton';
 
 interface IProps {
   githubSignInLink: string;
@@ -22,11 +22,9 @@ export class SignInModel extends Component<IProps> {
         <div className={'sign-in-content'}>
           <div className={'title'}>Sign In</div>
           <div className={'intro'}>
-            Please sign in so that we know the short links created
-            are yours.
+            Please sign in so that we know the short links created are yours.
           </div>
-          <GithubSignInButton
-            githubSignInLink={this.props.githubSignInLink}/>
+          <GithubSignInButton githubSignInLink={this.props.githubSignInLink} />
         </div>
       </Modal>
     );
