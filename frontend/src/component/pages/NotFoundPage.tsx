6de092429094f3c889a88eb5ea61bef916378813
Harry Liu <byliuyang11@gmail.com>
2019-10-16 00:11:15 -0700

    Format code

--- frontend/src/component/pages/NotFoundPage.tsx
+++ frontend/src/component/pages/NotFoundPage.tsx
@@ -5,9 +5,9 @@ export class NotFoundPage extends Component {
   render() {
     return (
       <div className={'page-404'}>
-        <div className="code">404</div>
-        <div className="to-home">
-          Take me back to <a href="/">homepage</a>.
+        <div className='code'>404</div>
+        <div className='to-home'>
+          Take me back to <a href='/'>homepage</a>.
         </div>
       </div>
     );
