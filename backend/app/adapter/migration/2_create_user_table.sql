d08fe0acd778f05db3dbb97d3574ac87beb16f6f
Harry Liu <byliuyang11@gmail.com>
2019-10-26 08:55:11 -0700

    Format backend code

--- backend/app/adapter/migration/2_create_user_table.sql
+++ backend/app/adapter/migration/2_create_user_table.sql
@@ -1,8 +1,9 @@
 -- +migrate Up
-CREATE TABLE "User" (
-    "email" CHARACTER VARYING(254) PRIMARY KEY,
-    "name" CHARACTER VARYING(80),
+CREATE TABLE "User"
+(
+    "email"          CHARACTER VARYING(254) PRIMARY KEY,
+    "name"           CHARACTER VARYING(80),
     "lastSignedInAt" TIMESTAMP WITH TIME ZONE,
-    "createdAt" TIMESTAMP WITH TIME ZONE,
-    "updatedAt" TIMESTAMP WITH TIME ZONE
+    "createdAt"      TIMESTAMP WITH TIME ZONE,
+    "updatedAt"      TIMESTAMP WITH TIME ZONE
 );
--- backend/app/adapter/migration/4_rename_user_table_and_columns.sql
+++ backend/app/adapter/migration/4_rename_user_table_and_columns.sql
@@ -1,7 +1,10 @@
 -- +migrate Up
 ALTER TABLE "User"
-RENAME TO "user";
+    RENAME TO "user";
 
-ALTER TABLE "user" RENAME COLUMN "lastSignedInAt" TO last_signed_in_at;
-ALTER TABLE "user" RENAME COLUMN "createdAt" TO created_at;
-ALTER TABLE "user" RENAME COLUMN "updatedAt" TO updated_at;
+ALTER TABLE "user"
+    RENAME COLUMN "lastSignedInAt" TO last_signed_in_at;
+ALTER TABLE "user"
+    RENAME COLUMN "createdAt" TO created_at;
+ALTER TABLE "user"
+    RENAME COLUMN "updatedAt" TO updated_at;
