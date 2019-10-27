45e6c90135e32e77b3d7c8174f5a6ba85c0b5332
Nikolay Norkin <nikolay.norkin@forward3d.com>
2019-10-27 13:23:41 +0000

    Disable FB login by default

--- frontend/src/service/StaticConfigDecision.service.ts
+++ frontend/src/service/StaticConfigDecision.service.ts
@@ -5,6 +5,6 @@ export class StaticConfigDecisionService implements IFeatureDecisionService {
     return false;
   }
   includeFacebookSignButton(): boolean {
-    return true;
+    return false;
   }
 }
