a93ab66d2722c7ee93f1765c2110a53805e6cc28
James Cote <3276350+Coteh@users.noreply.github.com>
2019-11-08 12:05:35 -0500

    Update backend/app/adapter/migration/8_create_public_url_table.sql
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>

--- backend/app/adapter/migration/8_create_public_url_table.sql
+++ backend/app/adapter/migration/8_create_public_url_table.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
 CREATE TABLE public_url (
-    alias CHARACTER VARYING,
+    alias CHARACTER VARYING(50)  NOT NULL,
     FOREIGN KEY (alias) REFERENCES url (alias) ON DELETE CASCADE ON UPDATE CASCADE
 );
