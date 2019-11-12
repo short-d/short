53c93621cef20025ed3ce7fab4c427862c6dd83a
Harry Liu <byliuyang11@gmail.com>
2019-11-12 09:05:20 -0800

    Update URL creator to accept user instead of email (#323)
    * Update URL creator to accept user instead of email
    
    * Clean up

---
+++ backend/app/adapter/db/userurl_test.go
@@ -0,0 +1,54 @@
+package db
+
+import (
+	"database/sql/driver"
+	"errors"
+	"fmt"
+	"short/app/adapter/db/table"
+	"short/app/entity"
+	"testing"
+
+	"github.com/byliuyang/app/mdtest"
+)
+
+func TestNewUserURLRelationSQL(t *testing.T) {
+	testCases := []struct {
+		name      string
+		user      entity.User
+		alias     string
+		expHasErr bool
+	}{
+		{
+			name:      "",
+			expHasErr: true,
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			db, stub, err := mdtest.NewSQLStub()
+			mdtest.Equal(t, nil, err)
+			defer db.Close()
+
+			statement := fmt.Sprintf(
+				`INSERT INTO "%s" (.+) VALUES (.+)`,
+				table.UserURLRelation.TableName,
+			)
+
+			if testCase.expHasErr {
+				stub.ExpectExec(statement).WillReturnError(errors.New("row exists"))
+			} else {
+				stub.ExpectExec(statement).WillReturnResult(driver.ResultNoRows)
+			}
+
+			userURLRelationRepo := NewUserURLRelationSQL(db)
+			err = userURLRelationRepo.CreateRelation(testCase.user, testCase.alias)
+
+			if testCase.expHasErr {
+				mdtest.NotEqual(t, nil, err)
+				return
+			}
+			mdtest.Equal(t, nil, err)
+		})
+	}
+}
