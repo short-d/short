5e9c896a672acd92bd2c4caa2e0289aa0b9ab40f
Harry Liu <byliuyang11@gmail.com>
2019-11-12 23:25:49 -0800

    Move input verification logic into usecase

--- backend/app/usecase/url/urlcreator.go
+++ backend/app/usecase/url/urlcreator.go
@@ -2,6 +2,7 @@ package url
 
 import (
 	"short/app/entity"
+	"short/app/usecase/input"
 	"short/app/usecase/keygen"
 	"short/app/usecase/repo"
 )
@@ -14,10 +15,22 @@ func (e ErrAliasExist) Error() string {
 	return string(e)
 }
 
+type ErrInvalidLongLink string
+
+func (e ErrInvalidLongLink) Error() string {
+	return string(e)
+}
+
+type ErrInvalidCustomAlias string
+
+func (e ErrInvalidCustomAlias) Error() string {
+	return string(e)
+}
+
 // Creator represents a URL alias creator
 type Creator interface {
-	CreateURL(url entity.URL, user entity.User) (entity.URL, error)
-	CreateURLWithCustomAlias(url entity.URL, alias string, user entity.User) (entity.URL, error)
+	CreateURL(url entity.URL, alias *string, user entity.User) (entity.URL, error)
+	//CreateURLWithCustomAlias(url entity.URL, alias string, user entity.User) (entity.URL, error)
 }
 
 // CreatorPersist represents a URL alias creator which persist the generated
@@ -26,24 +39,42 @@ type CreatorPersist struct {
 	urlRepo             repo.URL
 	userURLRelationRepo repo.UserURLRelation
 	keyGen              keygen.KeyGenerator
+	longLinkValidator   input.Validator
+	aliasValidator      input.Validator
+}
+
+func (c CreatorPersist) CreateURL(url entity.URL, customAlias *string, user entity.User) (entity.URL, error) {
+	longLink := url.OriginalURL
+	if !c.longLinkValidator.IsValid(&longLink) {
+		return entity.URL{}, ErrInvalidLongLink(longLink)
+	}
+
+	if customAlias == nil {
+		return c.createURLWithAutoAlias(url, user)
+	}
+
+	if !c.aliasValidator.IsValid(customAlias) {
+		return entity.URL{}, ErrInvalidCustomAlias(*customAlias)
+	}
+	return c.createURLWithCustomAlias(url, *customAlias, user)
 }
 
 // CreateURL persists a new url with a generated alias in the repository.
-func (a CreatorPersist) CreateURL(url entity.URL, user entity.User) (entity.URL, error) {
-	key, err := a.keyGen.NewKey()
+func (c CreatorPersist) createURLWithAutoAlias(url entity.URL, user entity.User) (entity.URL, error) {
+	key, err := c.keyGen.NewKey()
 	if err != nil {
 		return entity.URL{}, err
 	}
 	randomAlias := string(key)
-	return a.CreateURLWithCustomAlias(url, randomAlias, user)
+	return c.createURLWithCustomAlias(url, randomAlias, user)
 }
 
 // CreateURLWithCustomAlias persists a new url with a custom alias in
 // the repository.
-func (a CreatorPersist) CreateURLWithCustomAlias(url entity.URL, alias string, user entity.User) (entity.URL, error) {
+func (c CreatorPersist) createURLWithCustomAlias(url entity.URL, alias string, user entity.User) (entity.URL, error) {
 	url.Alias = alias
 
-	isExist, err := a.urlRepo.IsAliasExist(alias)
+	isExist, err := c.urlRepo.IsAliasExist(alias)
 	if err != nil {
 		return entity.URL{}, err
 	}
@@ -52,12 +83,12 @@ func (a CreatorPersist) CreateURLWithCustomAlias(url entity.URL, alias string, u
 		return entity.URL{}, ErrAliasExist("url alias already exist")
 	}
 
-	err = a.urlRepo.Create(url)
+	err = c.urlRepo.Create(url)
 	if err != nil {
 		return entity.URL{}, err
 	}
 
-	err = a.userURLRelationRepo.CreateRelation(user, url.Alias)
+	err = c.userURLRelationRepo.CreateRelation(user, url.Alias)
 	return url, err
 }
 
@@ -71,5 +102,7 @@ func NewCreatorPersist(
 		urlRepo:             urlRepo,
 		userURLRelationRepo: userURLRelationRepo,
 		keyGen:              keyGen,
+		longLinkValidator:   input.NewLongLink(),
+		aliasValidator:      input.NewCustomAlias(),
 	}
 }
--- backend/app/usecase/url/urlcreator_test.go
+++ backend/app/usecase/url/urlcreator_test.go
@@ -45,13 +45,15 @@ func TestURLCreatorPersist_CreateURL(t *testing.T) {
 				Email: "alpha@example.com",
 			},
 			url: entity.URL{
-				Alias:    "220uFicCJj",
-				ExpireAt: &now,
+				Alias:       "220uFicCJj",
+				OriginalURL: "https://www.google.com",
+				ExpireAt:    &now,
 			},
 			expHasErr: false,
 			expectedURL: entity.URL{
-				Alias:    "220uFicCJj",
-				ExpireAt: &now,
+				Alias:       "220uFicCJj",
+				OriginalURL: "https://www.google.com",
+				ExpireAt:    &now,
 			},
 		},
 	}
@@ -66,30 +68,13 @@ func TestURLCreatorPersist_CreateURL(t *testing.T) {
 
 			creator := NewCreatorPersist(&urlRepo, &userURLRepo, &keyGen)
 
-			urlsCopy := copyURLMap(testCase.urls)
-			fakeCreator := NewCreatorFake(urlsCopy, []string{
-				testCase.alias,
-			})
-
-			creators := []Creator{creator, fakeCreator}
-
-			for _, c := range creators {
-				url, err := c.CreateURL(testCase.url, testCase.user)
-				if testCase.expHasErr {
-					mdtest.NotEqual(t, nil, err)
-					return
-				}
-				mdtest.Equal(t, nil, err)
-				mdtest.Equal(t, testCase.expectedURL, url)
+			url, err := creator.CreateURL(testCase.url, &testCase.alias, testCase.user)
+			if testCase.expHasErr {
+				mdtest.NotEqual(t, nil, err)
+				return
 			}
+			mdtest.Equal(t, nil, err)
+			mdtest.Equal(t, testCase.expectedURL, url)
 		})
 	}
-}
-
-func copyURLMap(srcURLMap map[string]entity.URL) map[string]entity.URL {
-	urlsCopy := make(map[string]entity.URL)
-	for key, val := range srcURLMap {
-		urlsCopy[key] = val
-	}
-	return urlsCopy
 }
