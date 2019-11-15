6e9570102129dd0f21c388223540b9d84807943f
Harry Liu <byliuyang11@gmail.com>
2019-11-14 21:54:42 -0800

    Move URL fake to url package

--- backend/app/usecase/repo/url_fake.go
+++ backend/app/usecase/repo/url_fake.go
@@ -2,45 +2,51 @@ package repo
 
 import (
 	"errors"
-	"fmt"
 	"short/app/entity"
 )
 
 var _ URL = (*URLFake)(nil)
 
+// URLSql accesses URL information in url table through SQL.
 type URLFake struct {
 	urls map[string]entity.URL
 }
 
-func (u *URLFake) IsAliasExist(alias string) (bool, error) {
+// IsAliasExist checks whether a given alias exist in url table.
+func (u URLFake) IsAliasExist(alias string) (bool, error) {
 	_, ok := u.urls[alias]
 	return ok, nil
 }
 
+// Create inserts a new URL into url table.
 func (u *URLFake) Create(url entity.URL) error {
-	alias := url.Alias
-	url, ok := u.urls[alias]
-
-	if ok {
-		return errors.New(fmt.Sprintf("url exists (alias=%s)", alias))
+	isExist, err := u.IsAliasExist(url.Alias)
+	if err != nil {
+		return err
 	}
-
-	u.urls[alias] = url
+	if isExist {
+		return errors.New("alias exists")
+	}
+	u.urls[url.Alias] = url
 	return nil
 }
 
+// GetByAlias finds an URL in url table given alias.
 func (u URLFake) GetByAlias(alias string) (entity.URL, error) {
-	url, ok := u.urls[alias]
-
-	if !ok {
-		return entity.URL{}, errors.New(fmt.Sprintf("url not found (alias=%s)", alias))
+	isExist, err := u.IsAliasExist(alias)
+	if err != nil {
+		return entity.URL{}, err
 	}
-
+	if !isExist {
+		return entity.URL{}, errors.New("alias exists")
+	}
+	url := u.urls[alias]
 	return url, nil
 }
 
-func NewURLFake(urls map[string]entity.URL) URLFake {
-	return URLFake{
+// NewURLFake creates in memory URL repository
+func NewURLFake(urls map[string]entity.URL) *URLFake {
+	return &URLFake{
 		urls: urls,
 	}
 }
