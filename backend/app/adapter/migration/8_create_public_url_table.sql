325514c6058f9d2e759d279804dcc191391bd081
Harry Liu <byliuyang11@gmail.com>
2019-11-14 23:20:39 -0800

    Format SQL (#332)

--- backend/app/adapter/migration/8_create_public_url_table.sql
+++ backend/app/adapter/migration/8_create_public_url_table.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
 CREATE TABLE public_url (
-    alias CHARACTER VARYING(50)  NOT NULL,
+    alias CHARACTER VARYING(50) NOT NULL,
     FOREIGN KEY (alias) REFERENCES url (alias) ON DELETE CASCADE ON UPDATE CASCADE
 );
