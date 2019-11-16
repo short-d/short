419678c256520e36874cc40b3acae2452d226ecf
Harry Liu <byliuyang11@gmail.com>
2019-11-15 22:54:06 -0800

    Add comment

--- backend/app/adapter/graphql/resolver/User.go
+++ backend/app/adapter/graphql/resolver/User.go
@@ -4,10 +4,12 @@ import (
 	"short/app/entity"
 )
 
+// User stores user information used by GraphQL API
 type User struct {
 	user entity.User
 }
 
+// Email retrieves user's email address
 func (u User) Email() *string {
 	return &u.user.Email
 }
