511719ae2128a321c9e411879d5cddb579e6bb3e
jscodelover <manisha.4918@gmail.com>
2019-11-17 20:23:26 +0530

    feat: add sign out option Issue #305

--- frontend/src/component/pages/Home.tsx
+++ frontend/src/component/pages/Home.tsx
@@ -1,227 +1,236 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Home.scss';
 
-import {Header} from './shared/Header';
-import {Section} from '../ui/Section';
-import {TextField} from '../form/TextField';
-import {Button} from '../ui/Button';
-import {Url} from '../../entity/Url';
-import {Footer} from './shared/Footer';
-import {ShortLinkUsage} from './shared/ShortLinkUsage';
-import {SignInModal} from './shared/sign-in/SignInModal';
-import {Modal} from '../ui/Modal';
-import {ExtPromo} from './shared/promos/ExtPromo';
-import {CaptchaService} from '../../service/Captcha.service';
-import {validateLongLinkFormat} from '../../validators/LongLink.validator';
-import {validateCustomAliasFormat} from '../../validators/CustomAlias.validator';
-import {Location} from 'history';
-import {AuthService} from '../../service/Auth.service';
-import {VersionService} from '../../service/Version.service';
-import {QrCodeService} from '../../service/QrCode.service';
-import {UIFactory} from '../UIFactory';
-import {IAppState} from '../../state/reducers';
-import {Store} from 'redux';
+import { Header } from './shared/Header';
+import { Section } from '../ui/Section';
+import { TextField } from '../form/TextField';
+import { Button } from '../ui/Button';
+import { Url } from '../../entity/Url';
+import { Footer } from './shared/Footer';
+import { ShortLinkUsage } from './shared/ShortLinkUsage';
+import { SignInModal } from './shared/sign-in/SignInModal';
+import { Modal } from '../ui/Modal';
+import { ExtPromo } from './shared/promos/ExtPromo';
+import { CaptchaService } from '../../service/Captcha.service';
+import { validateLongLinkFormat } from '../../validators/LongLink.validator';
+import { validateCustomAliasFormat } from '../../validators/CustomAlias.validator';
+import { Location } from 'history';
+import { AuthService } from '../../service/Auth.service';
+import { VersionService } from '../../service/Version.service';
+import { QrCodeService } from '../../service/QrCode.service';
+import { UIFactory } from '../UIFactory';
+import { IAppState } from '../../state/reducers';
+import { Store } from 'redux';
 import {
-  clearError,
-  raiseCreateShortLinkError,
-  raiseInputError,
-  updateAlias,
-  updateCreatedUrl,
-  updateLongLink
+	clearError,
+	raiseCreateShortLinkError,
+	raiseInputError,
+	updateAlias,
+	updateCreatedUrl,
+	updateLongLink
 } from '../../state/actions';
-import {ErrorService} from '../../service/Error.service';
-import {IErr} from '../../entity/Err';
-import {UrlService} from '../../service/Url.service';
+import { ErrorService } from '../../service/Error.service';
+import { IErr } from '../../entity/Err';
+import { UrlService } from '../../service/Url.service';
 
 interface Props {
-  uiFactory: UIFactory;
-  urlService: UrlService;
-  authService: AuthService;
-  versionService: VersionService;
-  qrCodeService: QrCodeService;
-  captchaService: CaptchaService;
-  errorService: ErrorService;
-  store: Store<IAppState>;
-  location: Location;
+	uiFactory: UIFactory;
+	urlService: UrlService;
+	authService: AuthService;
+	versionService: VersionService;
+	qrCodeService: QrCodeService;
+	captchaService: CaptchaService;
+	errorService: ErrorService;
+	store: Store<IAppState>;
+	location: Location;
 }
 
 interface State {
-  longLink?: string;
-  alias?: string;
-  createdUrl?: Url;
-  qrCodeUrl?: string;
-  err?: IErr;
-  inputErr?: string;
+	longLink?: string;
+	alias?: string;
+	createdUrl?: Url;
+	qrCodeUrl?: string;
+	err?: IErr;
+	inputErr?: string;
 }
 
 export class Home extends Component<Props, State> {
-  errModal = React.createRef<Modal>();
-  signInModal = React.createRef<SignInModal>();
+	errModal = React.createRef<Modal>();
+	signInModal = React.createRef<SignInModal>();
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {};
-  }
+	constructor(props: Props) {
+		super(props);
+		this.state = {};
+	}
 
-  componentDidMount(): void {
-    this.props.authService.cacheAuthToken(this.props.location.search);
-    if (!this.props.authService.isSignedIn()) {
-      this.showSignInModal();
-      return;
-    }
+	componentDidMount(): void {
+		this.props.authService.cacheAuthToken(this.props.location.search);
+		if (!this.props.authService.isSignedIn()) {
+			this.showSignInModal();
+			return;
+		}
 
-    this.props.store.subscribe(async () => {
-      const state = this.props.store.getState();
+		this.props.store.subscribe(async () => {
+			const state = this.props.store.getState();
 
-      const newState: State = {
-        longLink: state.editingUrl.originalUrl,
-        alias: state.editingUrl.alias,
-        err: state.err,
-        createdUrl: state.createdUrl,
-        inputErr: state.inputErr,
-      };
+			const newState: State = {
+				longLink: state.editingUrl.originalUrl,
+				alias: state.editingUrl.alias,
+				err: state.err,
+				createdUrl: state.createdUrl,
+				inputErr: state.inputErr
+			};
 
-      if (state.createdUrl && state.createdUrl.alias) {
-        newState.qrCodeUrl = await this.props.qrCodeService.newQrCode(
-          this.props.urlService.aliasToFrontendLink(state.createdUrl.alias)
-        );
-      }
+			if (state.createdUrl && state.createdUrl.alias) {
+				newState.qrCodeUrl = await this.props.qrCodeService.newQrCode(
+					this.props.urlService.aliasToFrontendLink(state.createdUrl.alias)
+				);
+			}
 
-      if (newState.err) {
-        console.log(newState.err);
-        this.showError(newState.err);
-      }
-      this.setState(newState);
-    });
-  }
+			if (newState.err) {
+				console.log(newState.err);
+				this.showError(newState.err);
+			}
+			this.setState(newState);
+		});
+	}
 
-  showSignInModal() {
-    if (!this.signInModal.current) {
-      return;
-    }
-    this.signInModal.current.open();
-  }
+	handleSignOut = () => {
+		this.props.authService.signOut();
+		this.showSignInModal();
+	};
 
-  requestSignIn() {
-    this.props.authService.signOut();
-    this.showSignInModal();
-  }
+	showSignInModal() {
+		if (!this.signInModal.current) {
+			return;
+		}
+		this.signInModal.current.open();
+	}
 
-  handlerLongLinkChange = (newLongLink: string) => {
-    this.props.store.dispatch(updateLongLink(newLongLink));
-  };
+	requestSignIn() {
+		this.props.authService.signOut();
+		this.showSignInModal();
+	}
 
-  handleAliasChange = (newAlias: string) => {
-    this.props.store.dispatch(updateAlias(newAlias));
-  };
+	handlerLongLinkChange = (newLongLink: string) => {
+		this.props.store.dispatch(updateLongLink(newLongLink));
+	};
 
-  handleOnErrModalCloseClick = () => {
-    this.errModal.current!.close();
-    this.props.store.dispatch(clearError());
-  };
+	handleAliasChange = (newAlias: string) => {
+		this.props.store.dispatch(updateAlias(newAlias));
+	};
 
-  handlerLongLinkTextFieldBlur = () => {
-    let longLink = this.props.store.getState().editingUrl.originalUrl;
-    let err = validateLongLinkFormat(longLink);
-    this.props.store.dispatch(raiseInputError(err));
-  };
+	handleOnErrModalCloseClick = () => {
+		this.errModal.current!.close();
+		this.props.store.dispatch(clearError());
+	};
 
-  handlerCustomAliasTextFieldBlur = () => {
-    const alias = this.props.store.getState().editingUrl.alias;
-    const err = validateCustomAliasFormat(alias);
-    this.props.store.dispatch(raiseInputError(err));
-  };
+	handlerLongLinkTextFieldBlur = () => {
+		let longLink = this.props.store.getState().editingUrl.originalUrl;
+		let err = validateLongLinkFormat(longLink);
+		this.props.store.dispatch(raiseInputError(err));
+	};
 
-  handleCreateShortLinkClick = () => {
-    const editingUrl = this.props.store.getState().editingUrl;
-    this.props.urlService.createShortLink(editingUrl)
-      .then((createdUrl: Url) =>
-        this.props.store.dispatch(updateCreatedUrl(createdUrl))
-      )
-      .catch(({authorizationErr, createShortLinkErr}) => {
-        if (authorizationErr) {
-          this.requestSignIn();
-          return;
-        }
-        this.props.store.dispatch(raiseCreateShortLinkError(createShortLinkErr));
-      });
-  };
+	handlerCustomAliasTextFieldBlur = () => {
+		const alias = this.props.store.getState().editingUrl.alias;
+		const err = validateCustomAliasFormat(alias);
+		this.props.store.dispatch(raiseInputError(err));
+	};
 
-  showError(error?: IErr) {
-    if (!error) {
-      return;
-    }
-    this.errModal.current!.open();
-  }
+	handleCreateShortLinkClick = () => {
+		const editingUrl = this.props.store.getState().editingUrl;
+		this.props.urlService
+			.createShortLink(editingUrl)
+			.then((createdUrl: Url) =>
+				this.props.store.dispatch(updateCreatedUrl(createdUrl))
+			)
+			.catch(({ authorizationErr, createShortLinkErr }) => {
+				if (authorizationErr) {
+					this.requestSignIn();
+					return;
+				}
+				this.props.store.dispatch(
+					raiseCreateShortLinkError(createShortLinkErr)
+				);
+			});
+	};
 
-  render = () => {
-    return (
-      <div className="home">
-        <ExtPromo/>
-        <Header/>
-        <div className={'main'}>
-          <Section title={'New Short Link'}>
-            <div className={'control create-short-link'}>
-              <div className={'text-field-wrapper'}>
-                <TextField
-                  text={this.state.longLink}
-                  placeHolder={'Long Link'}
-                  onBlur={this.handlerLongLinkTextFieldBlur}
-                  onChange={this.handlerLongLinkChange}
-                />
-              </div>
-              <div className={'text-field-wrapper'}>
-                <TextField
-                  text={this.state.alias}
-                  placeHolder={'Custom Short Link ( Optional )'}
-                  onBlur={this.handlerCustomAliasTextFieldBlur}
-                  onChange={this.handleAliasChange}
-                />
-              </div>
-              <Button onClick={this.handleCreateShortLinkClick}>
-                Create Short Link
-              </Button>
-            </div>
-            <div className={'input-error'}>{this.state.inputErr}</div>
-            {this.state.createdUrl ? (
-              <div className={'short-link-usage-wrapper'}>
-                <ShortLinkUsage
-                  shortLink={this.props.urlService.aliasToFrontendLink(
-                    this.state.createdUrl.alias!
-                  )}
-                  originalUrl={this.state.createdUrl.originalUrl!}
-                  qrCodeUrl={this.state.qrCodeUrl!}
-                />
-              </div>
-            ) : (
-              false
-            )}
-          </Section>
-        </div>
-        <Footer
-          authorName={'Harry'}
-          authorPortfolio={'https://github.com/byliuyang'}
-          version={this.props.versionService.getAppVersion()}
-        />
+	showError(error?: IErr) {
+		if (!error) {
+			return;
+		}
+		this.errModal.current!.open();
+	}
 
-        <SignInModal
-          ref={this.signInModal}
-          uiFactory={this.props.uiFactory}
-        />
-        <Modal canClose={true} ref={this.errModal}>
-          {this.state.err ? <div className={'err'}>
-            <i
-              className={'material-icons close'}
-              title={'close'}
-              onClick={this.handleOnErrModalCloseClick}
-            >
-              close
-            </i>
-            <div className={'title'}>{this.state.err.name}</div>
-            <div className={'description'}>{this.state.err.description}</div>
-          </div> : false}
-        </Modal>
-      </div>
-    );
-  };
+	render = () => {
+		return (
+			<div className="home">
+				<ExtPromo />
+				<Header onSignOut={this.handleSignOut} />
+				<div className={'main'}>
+					<Section title={'New Short Link'}>
+						<div className={'control create-short-link'}>
+							<div className={'text-field-wrapper'}>
+								<TextField
+									text={this.state.longLink}
+									placeHolder={'Long Link'}
+									onBlur={this.handlerLongLinkTextFieldBlur}
+									onChange={this.handlerLongLinkChange}
+								/>
+							</div>
+							<div className={'text-field-wrapper'}>
+								<TextField
+									text={this.state.alias}
+									placeHolder={'Custom Short Link ( Optional )'}
+									onBlur={this.handlerCustomAliasTextFieldBlur}
+									onChange={this.handleAliasChange}
+								/>
+							</div>
+							<Button onClick={this.handleCreateShortLinkClick}>
+								Create Short Link
+							</Button>
+						</div>
+						<div className={'input-error'}>{this.state.inputErr}</div>
+						{this.state.createdUrl ? (
+							<div className={'short-link-usage-wrapper'}>
+								<ShortLinkUsage
+									shortLink={this.props.urlService.aliasToFrontendLink(
+										this.state.createdUrl.alias!
+									)}
+									originalUrl={this.state.createdUrl.originalUrl!}
+									qrCodeUrl={this.state.qrCodeUrl!}
+								/>
+							</div>
+						) : (
+							false
+						)}
+					</Section>
+				</div>
+				<Footer
+					authorName={'Harry'}
+					authorPortfolio={'https://github.com/byliuyang'}
+					version={this.props.versionService.getAppVersion()}
+				/>
+
+				<SignInModal ref={this.signInModal} uiFactory={this.props.uiFactory} />
+				<Modal canClose={true} ref={this.errModal}>
+					{this.state.err ? (
+						<div className={'err'}>
+							<i
+								className={'material-icons close'}
+								title={'close'}
+								onClick={this.handleOnErrModalCloseClick}
+							>
+								close
+							</i>
+							<div className={'title'}>{this.state.err.name}</div>
+							<div className={'description'}>{this.state.err.description}</div>
+						</div>
+					) : (
+						false
+					)}
+				</Modal>
+			</div>
+		);
+	};
 }
--- frontend/src/component/pages/shared/Header.tsx
+++ frontend/src/component/pages/shared/Header.tsx
@@ -1,14 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './Header.scss';
 
-export class Header extends Component {
-  render() {
-    return (
-      <header>
-        <div className={'center'}>
-          <div id="logo">Short</div>
-        </div>
-      </header>
-    );
-  }
+interface Props {
+	onSignOut: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export class Header extends Component<Props> {
+	render() {
+		return (
+			<header>
+				<div className={'center'}>
+					<div id="logo">Short</div>
+					<button className="sign-out-btn" onClick={this.props.onSignOut}>
+						Sign Out
+					</button>
+				</div>
+			</header>
+		);
+	}
 }
