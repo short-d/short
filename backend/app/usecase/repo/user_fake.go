e56d0b741c553390b136438810e5e56a4c25ad61
Harry Liu <byliuyang11@gmail.com>
2019-11-21 23:00:03 -0800

    Add fake user repository (#366)
    * Add fake user repository
    
    * Add comments

---
+++ backend/app/usecase/repo/user_fake.go
@@ -0,0 +1,51 @@
+package repo
+
+import (
+	"errors"
+	"short/app/entity"
+)
+
+var _ User = (*UserFake)(nil)
+
+// UserFake represents in memory implementation of user repository.
+type UserFake struct {
+	users []entity.User
+}
+
+// IsEmailExist checks whether an user with given email exist in the repository.
+func (u UserFake) IsEmailExist(email string) (bool, error) {
+	for _, user := range u.users {
+		if user.Email == email {
+			return true, nil
+		}
+	}
+	return false, nil
+}
+
+// GetUserByEmail find an user with a given email.
+func (u UserFake) GetUserByEmail(email string) (entity.User, error) {
+	for _, user := range u.users {
+		if user.Email == email {
+			return user, nil
+		}
+	}
+	return entity.User{}, errors.New("email not found")
+}
+
+// CreateUser creates and persists user in the repository for future access.
+func (u *UserFake) CreateUser(user entity.User) error {
+	for _, user := range u.users {
+		if user.Email == user.Email {
+			return errors.New("user exists")
+		}
+	}
+	u.users = append(u.users, user)
+	return nil
+}
+
+// NewUserFake create in memory user repository implementation.
+func NewUserFake(users []entity.User) UserFake {
+	return UserFake{
+		users: users,
+	}
+}
