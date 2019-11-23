eff9c0e66e436058915b832c2342f2398fd0c27c
Harry Liu <byliuyang11@gmail.com>
2019-11-23 00:21:18 -0800

    Update comment

--- backend/app/usecase/repository/url.go
+++ backend/app/usecase/repository/url.go
@@ -2,6 +2,7 @@ package repository
 
 import "short/app/entity"
 
+// URL accesses urls from storage, such as database.
 type URL interface {
 	IsAliasExist(alias string) (bool, error)
 	GetByAlias(alias string) (entity.URL, error)
