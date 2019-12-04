c0497206176b1d09951935e50699be472eb022f4
Harry Liu <byliuyang11@gmail.com>
2019-12-03 23:11:43 -0800

    Add account mapping repository (#410)
    * Add account mapping repository
    
    * Add comments

---
+++ backend/app/usecase/repository/accountmapping.go
@@ -0,0 +1,10 @@
+package repository
+
+import "short/app/entity"
+
+// AccountMapping accesses account mapping between SSOUser and internal User
+// from storage media, such as database.
+type AccountMapping interface {
+	IsSSOUserExist(ssoUser entity.SSOUser) (bool, error)
+	CreateMapping(ssoUser entity.SSOUser, user entity.User) error
+}
