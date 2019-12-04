a68706083261cca0edbe1a0cc05ed3a1181f5c03
Harry Liu <byliuyang11@gmail.com>
2019-12-03 23:30:49 -0800

    Rename LinkAccount to EnsureAndLinkAccount. Add comments

--- backend/app/usecase/account/linker.go
+++ backend/app/usecase/account/linker.go
@@ -6,17 +6,23 @@ import (
 	"short/app/usecase/repository"
 )
 
+// Linker provides account linking service.
 type Linker struct {
 	keyGen             keygen.KeyGenerator
 	userRepo           repository.User
 	accountMappingRepo repository.AccountMapping
 }
 
+// IsAccountLinked checks whether a given external account is linked to any
+// internal users already.
 func (l Linker) IsAccountLinked(ssoUser entity.SSOUser) (bool, error) {
 	return l.accountMappingRepo.IsSSOUserExist(ssoUser)
 }
 
-func (l Linker) LinkAccount(ssoUser entity.SSOUser) error {
+// EnsureAndLinkAccount creates an internal account when there is no internal
+// account sharing the same email as the given external account and link them
+// together afterwards.
+func (l Linker) EnsureAndLinkAccount(ssoUser entity.SSOUser) error {
 	isAccountLinked, err := l.IsAccountLinked(ssoUser)
 	if err != nil {
 		return err
@@ -72,6 +78,7 @@ func (l Linker) assignUserID(userEmail string, userID string) error {
 	return l.userRepo.UpdateUserID(userEmail, userID)
 }
 
+// NewLinker creates a new account linking service.
 func NewLinker(
 	keyGen keygen.KeyGenerator,
 	userRepo repository.User,
