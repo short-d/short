af0ff2f93e9916ff9f7ab7793e3984c55dc0a100
Harry Liu <byliuyang11@gmail.com>
2019-12-05 21:27:06 -0800

    Add comments

--- backend/dep/wire.go
+++ backend/dep/wire.go
@@ -22,7 +22,6 @@ import (
 	"github.com/byliuyang/app/fw"
 	"github.com/byliuyang/app/modern/mdcli"
 	"github.com/byliuyang/app/modern/mddb"
-	"github.com/byliuyang/app/modern/mdenv"
 	"github.com/byliuyang/app/modern/mdhttp"
 	"github.com/byliuyang/app/modern/mdlogger"
 	"github.com/byliuyang/app/modern/mdrequest"
@@ -59,6 +58,7 @@ var facebookAPISet = wire.NewSet(
 	facebook.NewAPI,
 )
 
+// InjectCommandFactory creates CommandFactory with configured dependencies.
 func InjectCommandFactory() fw.CommandFactory {
 	wire.Build(
 		wire.Bind(new(fw.CommandFactory), new(mdcli.CobraFactory)),
@@ -67,6 +67,7 @@ func InjectCommandFactory() fw.CommandFactory {
 	return mdcli.CobraFactory{}
 }
 
+// InjectDBConnector creates DBConnector with configured dependencies.
 func InjectDBConnector() fw.DBConnector {
 	wire.Build(
 		wire.Bind(new(fw.DBConnector), new(mddb.PostgresConnector)),
@@ -75,14 +76,7 @@ func InjectDBConnector() fw.DBConnector {
 	return mddb.PostgresConnector{}
 }
 
-func InjectEnvironment() fw.Environment {
-	wire.Build(
-		wire.Bind(new(fw.Environment), new(mdenv.GoDotEnv)),
-		mdenv.NewGoDotEnv,
-	)
-	return mdenv.GoDotEnv{}
-}
-
+// InjectDBMigrationTool creates DBMigrationTool with configured dependencies.
 func InjectDBMigrationTool() fw.DBMigrationTool {
 	wire.Build(
 		wire.Bind(new(fw.DBMigrationTool), new(mddb.PostgresMigrationTool)),
@@ -91,6 +85,16 @@ func InjectDBMigrationTool() fw.DBMigrationTool {
 	return mddb.PostgresMigrationTool{}
 }
 
+// InjectEnvironment creates Environment with configured dependencies.
+func InjectEnvironment() fw.Environment {
+	wire.Build(
+		wire.Bind(new(fw.Environment), new(mdenv.GoDotEnv)),
+		mdenv.NewGoDotEnv,
+	)
+	return mdenv.GoDotEnv{}
+}
+
+// InjectGraphQlService creates GraphQL service with configured dependencies.
 func InjectGraphQlService(
 	name string,
 	sqlDB *sql.DB,
@@ -134,6 +138,7 @@ func InjectGraphQlService(
 	return mdservice.Service{}, nil
 }
 
+// InjectRoutingService creates routing service with configured dependencies.
 func InjectRoutingService(
 	name string,
 	sqlDB *sql.DB,
