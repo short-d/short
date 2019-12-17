7b5cde80c5f1a63c4b69726427bc8d1f927e1846
Harry Liu <byliuyang11@gmail.com>
2019-12-16 21:36:19 -0800

    Add comment to ReCaptcha

--- backend/app/usecase/service/recaptcha.go
+++ backend/app/usecase/service/recaptcha.go
@@ -23,14 +23,17 @@ func (j *JSONTime) UnmarshalJSON(buf []byte) error {
 	return nil
 }
 
+// VerifyResponse represents the JSON response received from ReCaptcha Verify
+// API.
 type VerifyResponse struct {
 	Success       bool     `json:"success"`
-	ChallengeTime JSONTime `json:"challenge_ts"`
+	ChallengeTime string `json:"challenge_ts"`
 	Hostname      string   `json:"hostname"`
 	Score         float32  `json:"score"`
 	Action        string   `json:"action"`
 }
 
+// ReCaptcha verifies captcha response.
 type ReCaptcha interface {
 	Verify(recaptchaResponse string) (VerifyResponse, error)
 }
