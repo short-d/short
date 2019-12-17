8451760832a31d80d6d015d13860012739a607d5
Harry Liu <byliuyang11@gmail.com>
2019-12-16 21:00:16 -0800

    Add comments for request verifier

--- backend/app/usecase/requester/verifier.go
+++ backend/app/usecase/requester/verifier.go
@@ -4,10 +4,12 @@ import (
 	"short/app/usecase/service"
 )
 
+// Verifier verifies in coming network to prevents cyber attacks.
 type Verifier struct {
 	service service.ReCaptcha
 }
 
+// IsHuman checks whether the request is sent by human user.
 func (r Verifier) IsHuman(recaptchaResponse string) (bool, error) {
 	apiRes, err := r.service.Verify(recaptchaResponse)
 	if err != nil {
@@ -16,6 +18,7 @@ func (r Verifier) IsHuman(recaptchaResponse string) (bool, error) {
 	return apiRes.Score > 0.7, nil
 }
 
+// NewVerifier creates new request verifier.
 func NewVerifier(service service.ReCaptcha) Verifier {
 	return Verifier{
 		service: service,
