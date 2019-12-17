5e607e24921d505ae8cfbd86a1769ab34c36d6c2
Harry Liu <byliuyang11@gmail.com>
2019-12-16 22:05:26 -0800

    Remove used JSONTime

--- backend/app/usecase/service/recaptcha.go
+++ backend/app/usecase/service/recaptcha.go
@@ -1,28 +1,5 @@
 package service
 
-import (
-	"encoding/json"
-	"fmt"
-	"time"
-)
-
-type JSONTime time.Time
-
-func (j *JSONTime) UnmarshalJSON(buf []byte) error {
-	var timeStr string
-	err := json.Unmarshal(buf, &timeStr)
-	if err != nil {
-		return err
-	}
-	t, err := time.Parse(time.RFC3339, timeStr)
-	if err != nil {
-		return err
-	}
-	fmt.Println(t)
-	*j = JSONTime(t)
-	return nil
-}
-
 // VerifyResponse represents the JSON response received from ReCaptcha Verify
 // API.
 type VerifyResponse struct {
