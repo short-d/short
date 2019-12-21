5ea8a9fb9d28bc94f644db8efcb80890b903e546
tom <tom@toms-MacBook-Pro.local>
2019-12-20 20:08:06 -0800

    Add Google OAuth API in route.go

--- backend/app/adapter/routing/route.go
+++ backend/app/adapter/routing/route.go
@@ -3,6 +3,7 @@ package routing
 import (
 	netURL "net/url"
 	"short/app/adapter/facebook"
+	"short/app/adapter/google"
 	"short/app/adapter/github"
 	"short/app/usecase/account"
 	"short/app/usecase/auth"
@@ -27,6 +28,7 @@ func NewShort(
 	urlRetriever url.Retriever,
 	githubAPI github.API,
 	facebookAPI facebook.API,
+	googleAPI google.API,
 	authenticator auth.Authenticator,
 	accountProvider account.Provider,
 ) []fw.Route {
@@ -39,6 +41,12 @@ func NewShort(
 	facebookSignIn := sso.NewSingleSignOn(
 		facebookAPI.IdentityProvider,
 		facebookAPI.Account,
+		accountProvider,
+		authenticator,
+	)
+	googleSignIn := sso.NewSingleSignOn(
+		googleAPI.IdentityProvider,
+		googleAPI.Account,
 		accountProvider,
 		authenticator,
 	)
@@ -88,6 +96,27 @@ func NewShort(
 				logger,
 				tracer,
 				facebookSignIn,
+				*frontendURL,
+			),
+		},
+		{
+			Method: "GET",
+			Path:   "/oauth/google/sign-in",
+			Handle: NewSSOSignIn(
+				logger,
+				tracer,
+				googleAPI.IdentityProvider,
+				authenticator,
+				webFrontendURL,
+			),
+		},
+		{
+			Method: "GET",
+			Path:   "/oauth/google/sign-in/callback",
+			Handle: NewSSOSignInCallback(
+				logger,
+				tracer,
+				googleSignIn,
 				*frontendURL,
 			),
 		},
