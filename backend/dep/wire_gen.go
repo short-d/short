0d6dca4b9ad967923d504961497bcc20f6b83d7b
tom <tom@toms-MacBook-Pro.local>
2019-12-20 22:02:50 -0800

    update

--- backend/dep/wire_gen.go
+++ backend/dep/wire_gen.go
@@ -7,17 +7,6 @@ package dep
 
 import (
 	"database/sql"
-	"github.com/byliuyang/app/fw"
-	"github.com/byliuyang/app/modern/mdcli"
-	"github.com/byliuyang/app/modern/mddb"
-	"github.com/byliuyang/app/modern/mdhttp"
-	"github.com/byliuyang/app/modern/mdlogger"
-	"github.com/byliuyang/app/modern/mdrequest"
-	"github.com/byliuyang/app/modern/mdrouting"
-	"github.com/byliuyang/app/modern/mdservice"
-	"github.com/byliuyang/app/modern/mdtimer"
-	"github.com/byliuyang/app/modern/mdtracer"
-	"github.com/google/wire"
 	"short/app/adapter/db"
 	"short/app/adapter/facebook"
 	"short/app/adapter/github"
@@ -29,6 +18,18 @@ import (
 	"short/app/usecase/validator"
 	"short/dep/provider"
 	"time"
+
+	"github.com/byliuyang/app/fw"
+	"github.com/byliuyang/app/modern/mdcli"
+	"github.com/byliuyang/app/modern/mddb"
+	"github.com/byliuyang/app/modern/mdhttp"
+	"github.com/byliuyang/app/modern/mdlogger"
+	"github.com/byliuyang/app/modern/mdrequest"
+	"github.com/byliuyang/app/modern/mdrouting"
+	"github.com/byliuyang/app/modern/mdservice"
+	"github.com/byliuyang/app/modern/mdtimer"
+	"github.com/byliuyang/app/modern/mdtracer"
+	"github.com/google/wire"
 )
 
 // Injectors from wire.go:
