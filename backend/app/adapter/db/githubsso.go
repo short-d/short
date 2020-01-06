e8cc0ce471326d36d9d86279a70d8c74bf0dafd3
Harry Liu <byliuyang11@gmail.com>
2020-01-05 23:23:32 -0800

    Add IsSSOUserExist for githubSSORepo

---
+++ backend/app/adapter/db/githubsso.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/short-d/app/fw"
+	"github.com/short-d/short/app/adapter/db/table"
+	"github.com/short-d/short/app/entity"
+	"github.com/short-d/short/app/usecase/repository"
+)
+
+var _ repository.AccountMapping = (*GithubSSOSql)(nil)
+
+type GithubSSOSql struct {
+	db     *sql.DB
+	logger fw.Logger
+}
+
+func (g GithubSSOSql) IsSSOUserExist(ssoUser entity.SSOUser) (bool, error) {
+	query := fmt.Sprintf(`
+SELECT "%s"
+FROM "%s"
+WHERE "%s"=$1;
+`,
+		table.GithubSSO.ColumnGithubUserID,
+		table.GithubSSO.TableName,
+		table.GithubSSO.ColumnGithubUserID,
+	)
+	var id string
+	err := g.db.QueryRow(query, ssoUser.ID).Scan(&id)
+	if err == nil {
+		return true, err
+	}
+	if err == sql.ErrNoRows {
+		return false, nil
+	}
+	g.logger.Error(err)
+	return false, err
+}
+
+func (g GithubSSOSql) CreateMapping(ssoUser entity.SSOUser, user entity.User) error {
+	panic("implement me")
+}
+
+func NewGithubSSOSql(db *sql.DB, logger fw.Logger) GithubSSOSql {
+	return GithubSSOSql{db: db, logger: logger}
+}
