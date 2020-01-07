ea648da35e401cbb1ed13cc1d4d8df5b7e81c144
Harry Liu <byliuyang11@gmail.com>
2020-01-06 20:36:30 -0800

    Fix comments

--- backend/dep/provider/logger.go
+++ backend/dep/provider/logger.go
@@ -5,8 +5,13 @@ import (
 	"github.com/short-d/app/modern/mdlogger"
 )
 
+// LogPrefix represents the prefix of a log message.
+// "[Short]" is the prefix part of the following log message:
+// [Short] [Info] 2020-01-07 04:33:22 line 25 at service.go GraphQL API started
 type LogPrefix string
 
+// NewLocalLogger creates local logger with LogPrefix to uniquely identify log
+// prefix during dependency injection.
 func NewLocalLogger(
 	prefix LogPrefix,
 	level fw.LogLevel,
