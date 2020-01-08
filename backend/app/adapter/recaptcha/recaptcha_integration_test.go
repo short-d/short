3c5ded8936b058501283f0826d3c905afd42c7b0
Harry Liu <byliuyang11@gmail.com>
2020-01-08 00:21:59 -0800

    Support retrieving Github user ID (#478)
    * Upgrade app framework version
    
    * Support retrieving Github user ID
    
    * Update app framework version
    
    * Fix recaptcha integration test
    
    * Add integration test for Github account API

--- backend/app/adapter/recaptcha/recaptcha_integration_test.go
+++ backend/app/adapter/recaptcha/recaptcha_integration_test.go
@@ -3,49 +3,53 @@
 package recaptcha
 
 import (
+	"bytes"
 	"io/ioutil"
 	"net/http"
 	"net/url"
 	"testing"
 
 	"github.com/short-d/app/mdtest"
-	"github.com/short-d/app/modern/mdrequest"
 	"github.com/short-d/short/app/usecase/service"
-	//"time"
 )
 
 func TestReCaptcha_Verify(t *testing.T) {
 	expSecret := "ZPDIGNFj1EQJeNfs"
 	expCaptchaResponse := "qHwha3zZh9G9mquEUOKZ"
-	//now := time.Now()
 
 	testCases := []struct {
-		name        string
-		apiResponse map[string]interface{}
-		expRes      service.VerifyResponse
+		name         string
+		httpResponse *http.Response
+		httpErr      error
+		expRes       service.VerifyResponse
 	}{
 		{
 			name: "successful request with score = 0.8",
-			apiResponse: map[string]interface{}{
-				"success": true,
-				"action":  "homepage",
-				"score":   0.8,
-				//"challenge_ts": now.Format(time.RFC3339),
-				"hostname": "s.time4hacks.com",
-			},
+			httpResponse: &http.Response{
+				StatusCode: http.StatusOK,
+				Body: ioutil.NopCloser(bytes.NewReader([]byte(`
+{
+	"success": true,
+	"action":  "homepage",
+	"score":   0.8,
+	"challenge_ts": "2006-01-02T15:04:05+07:00",
+	"hostname": "s.time4hacks.com"
+}
+`,
+				)))},
 			expRes: service.VerifyResponse{
-				Success: true,
-				Action:  "homepage",
-				Score:   0.8,
-				//ChallengeTime: service.JSONTime(now),
-				Hostname: "s.time4hacks.com",
+				Success:       true,
+				Action:        "homepage",
+				Score:         0.8,
+				ChallengeTime: "2006-01-02T15:04:05+07:00",
+				Hostname:      "s.time4hacks.com",
 			},
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
-			transport := mdtest.NewTransportMock(func(req *http.Request) (response *http.Response, e error) {
+			httpRequest := mdtest.NewHTTPRequestFake(func(req *http.Request) (response *http.Response, e error) {
 				mdtest.Equal(t, "https://www.google.com/recaptcha/api/siteverify", req.URL.String())
 				mdtest.Equal(t, "POST", req.Method)
 				mdtest.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
@@ -58,16 +62,12 @@ func TestReCaptcha_Verify(t *testing.T) {
 
 				mdtest.Equal(t, expSecret, params.Get("secret"))
 				mdtest.Equal(t, expCaptchaResponse, params.Get("response"))
-				return mdtest.JSONResponse(testCase.apiResponse)
+				return testCase.httpResponse, testCase.httpErr
 			})
 
-			client := http.Client{
-				Transport: transport,
-			}
-			req := mdrequest.NewHTTP(client)
-
-			rc := NewService(req, expSecret)
+			rc := NewService(httpRequest, expSecret)
 			gotRes, err := rc.Verify(expCaptchaResponse)
+
 			mdtest.Equal(t, nil, err)
 			mdtest.Equal(t, testCase.expRes, gotRes)
 		})
