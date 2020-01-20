81e5299ecd8e5284fc3b7a624fda7ff853b225f9
Harry Liu <byliuyang11@gmail.com>
2020-01-20 10:45:55 -0800

    Fix syntax mistake

--- backend/app/usecase/presenter.go
+++ backend/app/usecase/presenter.go
@@ -1,6 +1,6 @@
 package usecase
 
-// Presenter transforms data into proper format and passes it to view layer.
+// Presenter transforms data into proper format and passes it to the view layer.
 type Presenter interface {
 	ShowHome()
 	ShowUserHome(authToken string)
