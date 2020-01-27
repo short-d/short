23de27cc293bcb6849485776cbea14b9a6d856ce
dawntomm <dawntomm@gmail.com>
2020-01-27 00:49:40 -0800

    Refactor IdentityProvider for Facebook OAuth (#501)
    * Refactor IdentityProvider for Facebook OAuth
    
    * update
    
    * add integration test
    
    * address comment
    
    * address comment
    
    * address comment
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

---
+++ backend/app/adapter/facebook/identityprovider_integration_test.go
@@ -0,0 +1,153 @@
+// +build integration all
+
+package facebook
+
+import (
+	"bytes"
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/short-d/app/mdtest"
+)
+
+func TestIdentityProvider_GetAuthorizationURL(t *testing.T) {
+	t.Parallel()
+	httpRequest := mdtest.NewHTTPRequestFake(
+		func(req *http.Request) (response *http.Response, e error) {
+			return nil, nil
+		})
+	clientID := "id_12345"
+	clientSecret := "client_secret"
+	redirectURI := "http://localhost/oauth/facebook/sign-in/callback"
+	identityProvider := NewIdentityProvider(httpRequest, clientID, clientSecret, redirectURI)
+
+	urlResponse := identityProvider.GetAuthorizationURL()
+
+	parsedUrl, err := url.Parse(urlResponse)
+
+	mdtest.Equal(t, nil, err)
+	mdtest.Equal(t, "https", parsedUrl.Scheme)
+	mdtest.Equal(t, "www.facebook.com", parsedUrl.Host)
+	mdtest.Equal(t, "/v4.0/dialog/oauth", parsedUrl.Path)
+	mdtest.Equal(t, "code", parsedUrl.Query().Get("response_type"))
+	mdtest.Equal(t, clientID, parsedUrl.Query().Get("client_id"))
+	mdtest.Equal(t, redirectURI, parsedUrl.Query().Get("redirect_uri"))
+
+	expectedScope := []string{"public_profile", "email"}
+	actualScope := strings.Split(parsedUrl.Query().Get("scope"), ",")
+	mdtest.SameElements(t, expectedScope, actualScope)
+}
+
+func TestIdentityProvider_RequestAccessToken(t *testing.T) {
+	testCases := []struct {
+		name                string
+		httpResponse        *http.Response
+		httpErr             error
+		clientID            string
+		clientSecret        string
+		redirectURI         string
+		authorizationCode   string
+		expectHasErr        bool
+		expectedAccessToken string
+	}{
+		{
+			name:                "invalid authorization code",
+			httpResponse:        nil,
+			httpErr:             errors.New("invalid authorization code"),
+			clientID:            "id_12345",
+			clientSecret:        "client_secret",
+			redirectURI:         "http://localhost/oauth/facebook/sign-in/callback",
+			authorizationCode:   "invalidCode",
+			expectHasErr:        true,
+			expectedAccessToken: "",
+		},
+		{
+			name:                "invalid clientID",
+			httpResponse:        nil,
+			httpErr:             errors.New("invalid clientID"),
+			clientID:            "invalidID",
+			clientSecret:        "client_secret",
+			redirectURI:         "http://localhost/oauth/facebook/sign-in/callback",
+			authorizationCode:   "authorizationCode_1",
+			expectHasErr:        true,
+			expectedAccessToken: "",
+		},
+		{
+			name:                "invalid client secret",
+			httpResponse:        nil,
+			httpErr:             errors.New("invalid clientSecret"),
+			clientID:            "id_12345",
+			clientSecret:        "invalidSecret",
+			redirectURI:         "http://localhost/oauth/facebook/sign-in/callback",
+			authorizationCode:   "authorizationCode_1",
+			expectHasErr:        true,
+			expectedAccessToken: "",
+		},
+		{
+			name:                "invalid redirect URI",
+			httpResponse:        nil,
+			httpErr:             errors.New("invalid redirectURI"),
+			clientID:            "id_12345",
+			clientSecret:        "client_secret",
+			redirectURI:         "http://localhost/invalidRedirectURI",
+			authorizationCode:   "authorizationCode_1",
+			expectHasErr:        true,
+			expectedAccessToken: "",
+		},
+		{
+			name: "success",
+			httpResponse: &http.Response{
+				StatusCode: http.StatusOK,
+				Body: ioutil.NopCloser(bytes.NewReader([]byte(`
+{
+	"access_token": "bcBi3AMeOV3Zg3AlOPyn",
+	"token_type": "bearer",
+	"expires_in": 5183944
+}
+`,
+				)))},
+			httpErr:             nil,
+			clientID:            "id_12345",
+			clientSecret:        "client_secret",
+			redirectURI:         "http://localhost/oauth/facebook/sign-in/callback",
+			authorizationCode:   "authorizationCode_1",
+			expectHasErr:        false,
+			expectedAccessToken: "bcBi3AMeOV3Zg3AlOPyn",
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+			httpRequest := mdtest.NewHTTPRequestFake(
+				func(req *http.Request) (response *http.Response, e error) {
+					mdtest.Equal(t, "https", req.URL.Scheme)
+					mdtest.Equal(t, "graph.facebook.com", req.URL.Host)
+					mdtest.Equal(t, "/v4.0/oauth/access_token", req.URL.Path)
+					mdtest.Equal(t, testCase.clientID, req.URL.Query().Get("client_id"))
+					mdtest.Equal(t, testCase.clientSecret, req.URL.Query().Get("client_secret"))
+					mdtest.Equal(t, testCase.authorizationCode, req.URL.Query().Get("code"))
+					mdtest.Equal(t, testCase.redirectURI, req.URL.Query().Get("redirect_uri"))
+					mdtest.Equal(t, "POST", req.Method)
+					mdtest.Equal(t, "application/json", req.Header.Get("Accept"))
+					mdtest.Equal(t, "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+
+					return testCase.httpResponse, testCase.httpErr
+				})
+			identityProvider := NewIdentityProvider(httpRequest, testCase.clientID, testCase.clientSecret, testCase.redirectURI)
+			actualAccessToken, err := identityProvider.RequestAccessToken(testCase.authorizationCode)
+
+			if testCase.expectHasErr {
+				mdtest.NotEqual(t, nil, err)
+				return
+			}
+			mdtest.Equal(t, nil, err)
+			mdtest.Equal(t, testCase.expectedAccessToken, actualAccessToken)
+		})
+	}
+}
