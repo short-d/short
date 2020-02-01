89bb7a504d59851657554f6b29e62c3f6223cadb
Harry Liu <byliuyang11@gmail.com>
2020-02-01 10:50:16 -0800

    Move request Facebook sign in to use case

--- backend/app/usecase/usecase_test.go
+++ backend/app/usecase/usecase_test.go
@@ -161,9 +161,146 @@ func TestShort_RequestGithubSignIn(t *testing.T) {
 				testCase.now,
 				testCase.tokenValidDuration,
 				testCase.githubIDProvider,
+				stubIDProvider{},
 			)
 			presenter := newMockPresenter()
 			useCase.RequestGithubSignIn(testCase.authToken, &presenter)
+
+			mdtest.Equal(t, testCase.expectedShowHomeCallArgs, presenter.showHomeCallArgs)
+			mdtest.Equal(t, testCase.expectedShowUserHomeCallArgs, presenter.showUserHomeCallArgs)
+			mdtest.Equal(t, testCase.expectedShowExternalPageCallArgs, presenter.showExternalPageCallArgs)
+		})
+	}
+}
+
+func TestShort_RequestFacebookSignIn(t *testing.T) {
+	now, err := time.Parse(time.RFC3339, "2020-01-26T08:32:40.759788656Z")
+	mdtest.Equal(t, nil, err)
+
+	testCases := []struct {
+		name                             string
+		now                              time.Time
+		existingURLs                     map[string]entity.URL
+		existingUsers                    []entity.User
+		oauthURL                         string
+		facebookIDProvider               stubIDProvider
+		authToken                        string
+		tokenValidDuration               time.Duration
+		expectedShowHomeCallArgs         []showHomeCallArgs
+		expectedShowUserHomeCallArgs     []showUserHomeCallArgs
+		expectedShowExternalPageCallArgs []showExternalPageCallArgs
+	}{
+		{
+			name: "user already signed in",
+			now:  now,
+			facebookIDProvider: stubIDProvider{
+				authorizationURL: "facebook_sign_in_link",
+				accessToken:      "access_token",
+			},
+			authToken:                    "auth_token",
+			tokenValidDuration:           time.Hour,
+			expectedShowHomeCallArgs:     []showHomeCallArgs{},
+			expectedShowUserHomeCallArgs: []showUserHomeCallArgs{},
+			expectedShowExternalPageCallArgs: []showExternalPageCallArgs{
+				{
+					link: "facebook_sign_in_link",
+				},
+			},
+		},
+		{
+			name: "user has not signed in",
+			now:  now,
+			facebookIDProvider: stubIDProvider{
+				authorizationURL: "facebook_sign_in_link",
+				accessToken:      "access_token",
+			},
+			authToken:                    "",
+			tokenValidDuration:           time.Hour,
+			expectedShowHomeCallArgs:     []showHomeCallArgs{},
+			expectedShowUserHomeCallArgs: []showUserHomeCallArgs{},
+			expectedShowExternalPageCallArgs: []showExternalPageCallArgs{
+				{
+					link: "facebook_sign_in_link",
+				},
+			},
+		},
+		{
+			name: "auth token has no email",
+			now:  now,
+			facebookIDProvider: stubIDProvider{
+				authorizationURL: "facebook_sign_in_link",
+				accessToken:      "access_token",
+			},
+			authToken: `
+{
+  "issued_at": "2020-01-26T08:32:40.759788656Z"
+}
+`,
+			tokenValidDuration:           time.Hour,
+			expectedShowHomeCallArgs:     []showHomeCallArgs{},
+			expectedShowUserHomeCallArgs: []showUserHomeCallArgs{},
+			expectedShowExternalPageCallArgs: []showExternalPageCallArgs{
+				{
+					link: "facebook_sign_in_link",
+				},
+			},
+		},
+		{
+			name: "auth token has empty email",
+			now:  now,
+			facebookIDProvider: stubIDProvider{
+				authorizationURL: "facebook_sign_in_link",
+				accessToken:      "access_token",
+			},
+			authToken: `
+{
+  "email": "",
+  "issued_at": "2020-01-26T08:32:40.759788656Z"
+}
+`,
+			tokenValidDuration:           time.Hour,
+			expectedShowHomeCallArgs:     []showHomeCallArgs{},
+			expectedShowUserHomeCallArgs: []showUserHomeCallArgs{},
+			expectedShowExternalPageCallArgs: []showExternalPageCallArgs{
+				{
+					link: "facebook_sign_in_link",
+				},
+			},
+		},
+		{
+			name: "auth token expired",
+			now:  now,
+			facebookIDProvider: stubIDProvider{
+				authorizationURL: "facebook_sign_in_link",
+				accessToken:      "access_token",
+			},
+			authToken: `
+{
+  "email": "byliuyang11@gmail.com",
+  "issued_at": "2020-01-26T07:31:40.759788656Z"
+}
+`,
+			tokenValidDuration:           time.Hour,
+			expectedShowHomeCallArgs:     []showHomeCallArgs{},
+			expectedShowUserHomeCallArgs: []showUserHomeCallArgs{},
+			expectedShowExternalPageCallArgs: []showExternalPageCallArgs{
+				{
+					link: "facebook_sign_in_link",
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			useCase := newUseCase(
+				testCase.now,
+				testCase.tokenValidDuration,
+				stubIDProvider{},
+				testCase.facebookIDProvider,
+			)
+			presenter := newMockPresenter()
+			useCase.RequestFacebookSignIn(testCase.authToken, &presenter)
 
 			mdtest.Equal(t, testCase.expectedShowHomeCallArgs, presenter.showHomeCallArgs)
 			mdtest.Equal(t, testCase.expectedShowUserHomeCallArgs, presenter.showUserHomeCallArgs)
@@ -176,6 +313,7 @@ func newUseCase(
 	now time.Time,
 	tokenValidDuration time.Duration,
 	githubIDProvider GithubIDProvider,
+	facebookIDProvider FacebookIDProvider,
 ) UseCase {
 	logger := mdtest.NewLoggerFake(mdtest.FakeLoggerArgs{})
 	timer := mdtest.NewTimerFake(now)
@@ -187,6 +325,7 @@ func newUseCase(
 		timer,
 		authenticator,
 		githubIDProvider,
+		facebookIDProvider,
 	)
 	return useCase
 }
