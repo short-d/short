ece19ffc998e394041b1b3fca020663da754361d
James Cote <3276350+Coteh@users.noreply.github.com>
2020-03-08 19:50:34 -0400

    Add toggle component (#534)
    * Add toggle slider component
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Removed toggle-label
    
    * Remove test method from Home.tsx
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Format Toggle.tsx
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

---
+++ frontend/src/component/ui/Toggle.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+import './Toggle.scss';
+import classNames from 'classnames';
+
+interface Props {
+  onClick?: (enabled: boolean) => void;
+}
+
+interface State {
+  enabled: boolean;
+  buttonClassName: string;
+  backgroundClassName: string;
+}
+
+export class Toggle extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      enabled: false,
+      buttonClassName: classNames('button'),
+      backgroundClassName: classNames('background')
+    };
+  }
+
+  handleClick = () => {
+    this.setState(
+      {
+        enabled: !this.state.enabled
+      },
+      () => {
+        const { enabled } = this.state;
+        if (!this.props.onClick) {
+          return;
+        }
+        this.props.onClick(enabled);
+        if (enabled) {
+          this.setState({
+            buttonClassName: classNames('button', 'active'),
+            backgroundClassName: classNames('background', 'active')
+          });
+          return;
+        }
+        this.setState({
+          buttonClassName: classNames('button'),
+          backgroundClassName: classNames('background')
+        });
+      }
+    );
+  };
+
+  render() {
+    return (
+      <div className={'toggle'}>
+        {this.props.children}
+        <div
+          className={this.state.backgroundClassName}
+          onClick={this.handleClick}
+        >
+          <div className={this.state.buttonClassName}></div>
+        </div>
+      </div>
+    );
+  }
+}
