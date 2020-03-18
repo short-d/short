3d4b7748784baa4d136362370c50d9b8d7905dd0
kapillamba4 <kapillamba4@gmail.com>
2020-03-18 22:34:12 +0530

    Make requested changes

--- frontend/src/component/ui/ViewChangeLogButton.spec.tsx
+++ frontend/src/component/ui/ViewChangeLogButton.spec.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { ViewChangeLogButton } from './ViewChangeLogButton';
 
-it('renders without crashing and handles click events correctly', () => {
+it('renders without crashing', () => {
+  render(<ViewChangeLogButton />);
+});
+
+it('handles click events correctly', () => {
   const clickHandler = jest.fn();
   const { getByText } = render(<ViewChangeLogButton onClick={clickHandler} />);
   fireEvent.click(getByText('View Changelog'));
