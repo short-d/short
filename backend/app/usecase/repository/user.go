393317d490cd6322d1b890b2dab0eaac578f1aba
Harry Liu <byliuyang11@gmail.com>
2020-03-17 20:06:12 -0700

    Add GetUserByID to User repo

--- backend/app/usecase/repository/user.go
+++ backend/app/usecase/repository/user.go
@@ -6,6 +6,7 @@ import "github.com/short-d/short/app/entity"
 type User interface {
 	IsIDExist(id string) (bool, error)
 	IsEmailExist(email string) (bool, error)
+	GetUserByID(id string) (entity.User, error)
 	GetUserByEmail(email string) (entity.User, error)
 	CreateUser(user entity.User) error
 	UpdateUserID(email string, userID string) error
