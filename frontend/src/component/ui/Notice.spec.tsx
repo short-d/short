f25d6d7dd3ec03f2534c25b723998208ff43a451
Harry Liu <byliuyang11@gmail.com>
2020-03-20 09:39:02 -0700

    Apply suggestions from code review

--- frontend/src/component/ui/Notice.spec.tsx
+++ frontend/src/component/ui/Notice.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Notice } from './Notice';
 
-it('renders without crashing', () => {
+it('renders content correctly', () => {
   const { container } = render(
     <Notice>
       <div>
@@ -11,5 +11,6 @@ it('renders without crashing', () => {
       </div>
     </Notice>
   );
-  expect(container.textContent).toMatch('Lorem ipsum');
+  expect(container.textContent).toMatch('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua');
 });
