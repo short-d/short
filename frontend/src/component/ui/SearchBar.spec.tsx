0d66a93b138b8e1c7c35a6bee6cf4870bbdeb2f7
kapillamba4 <kapillamba4@gmail.com>
2020-03-20 15:28:27 +0530

    Add unit tests for SearchBar

---
+++ frontend/src/component/ui/SearchBar.spec.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { SearchBar } from './SearchBar';
+
+const DEBOUNCE_DURATION: number = 300;
+
+it('renders without crashing', () => {
+  const changeHandler = jest.fn();
+  render(<SearchBar onChange={changeHandler} />);
+});
+
+it('triggers change events successfully', async () => {
+  const changeHandler = jest.fn();
+  const { getByPlaceholderText } = render(
+    <SearchBar onChange={changeHandler} />
+  );
+  const input = getByPlaceholderText('Search short links') as HTMLInputElement;
+
+  expect(input.value).toBe('');
+
+  fireEvent.change(input, {
+    target: { value: 'Lorem ipsum' }
+  });
+
+  expect(changeHandler).toBeCalledTimes(0);
+
+  await new Promise(r => setTimeout(r, DEBOUNCE_DURATION));
+
+  expect(changeHandler).toBeCalledTimes(1);
+  expect(changeHandler).toBeCalledWith('Lorem ipsum');
+});
