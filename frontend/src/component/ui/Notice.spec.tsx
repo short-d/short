b65c1172176a319a9a6aaf01fb0a405afa9ad699
kapillamba4 <kapillamba4@gmail.com>
2020-03-20 13:52:16 +0530

    Add unit tests for Notice

---
+++ frontend/src/component/ui/Notice.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Notice } from './Notice';
+
+it('renders without crashing', () => {
+  const { container } = render(
+    <Notice>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua
+      </div>
+    </Notice>
+  );
+  expect(container.textContent).toMatch('Lorem ipsum');
+});
