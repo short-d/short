451ccfe9a35052c013437687e5f330a75dc850a0
kapillamba4 <kapillamba4@gmail.com>
2020-03-20 13:59:12 +0530

    Add unit tests for Section

---
+++ frontend/src/component/ui/Section.spec.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Section } from './Section';
+
+it('renders without crashing', () => {
+  const { container } = render(
+    <Section title="Section Title">
+        <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+        </div>
+    </Section>
+  );
+  expect(container.textContent).toMatch('Section Title');
+  expect(container.textContent).toMatch('Lorem ipsum');
+});
