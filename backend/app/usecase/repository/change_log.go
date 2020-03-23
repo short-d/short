5ac96d8f0ac32f1876a5b80d7797439cace3055f
kapillamba4 <kapillamba4@gmail.com>
2020-03-23 13:08:08 +0530

    Update repository/change_log.go

--- backend/app/usecase/repository/change_log.go
+++ backend/app/usecase/repository/change_log.go
@@ -6,6 +6,7 @@ import (
 
 // ChangeLog accesses changelog from storage, such as database.
 type ChangeLog interface {
+	GetChangeByID(id string) (entity.Change, error)
 	GetChangeLog() ([]entity.Change, error)
 	CreateChange(newChange entity.Change) (entity.Change, error)
 }
