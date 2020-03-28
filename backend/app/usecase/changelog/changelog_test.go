c286b80dc1dae6111074e3721090ed862153e533
Kapil Lamba <kapillamba4@gmail.com>
2020-03-28 13:30:59 +0530

    Add ChangeLog usecase (#602)
    * Add ChangeLog repository, ChangeLogSQL, DB migration with tests
    
    * Add comments
    
    * Update repository/change_log.go
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Update backend/app/adapter/db/change_log_integration_test.go
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Make requested changes
    
    * Small change
    
    * Make requested changes
    
    * Update backend/app/adapter/db/change_log_integration_test.go
    
    * Add changelog usecase with unit tests
    
    * Small change
    
    * Remove comments
    
    * Add comments
    
    * Add comments
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Make requested changes
    
    * Allow summary of a change to be empty
    
    * Allow summary to be nil
    
    * Small change
    
    * Add test case for nil summary
    
    * Format
    
    * Make requested changes
    
    * Format code
    
    * Small change
    
    * Apply suggestions from code review
    
    * Check if Persist implements ChangeLog interface
    
    * Update changelog.go
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

---
+++ backend/app/usecase/changelog/changelog_test.go
@@ -0,0 +1,223 @@
+// +build !integration all
+
+package changelog
+
+import (
+	"testing"
+	"time"
+
+	"github.com/short-d/app/mdtest"
+	"github.com/short-d/short/app/entity"
+	"github.com/short-d/short/app/usecase/keygen"
+	"github.com/short-d/short/app/usecase/repository"
+	"github.com/short-d/short/app/usecase/service"
+)
+
+func TestPersist_CreateChange(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now()
+	summaryMarkdown1 := "summary 1"
+	summaryMarkdown2 := "summary 2"
+	summaryMarkdown3 := "summary 3"
+	testCases := []struct {
+		name                  string
+		changeLog             []entity.Change
+		change                entity.Change
+		expectedChange        entity.Change
+		availableKeys         []service.Key
+		expectedChangeLogSize int
+		hasErr                bool
+	}{
+		{
+			name: "create change successfully",
+			changeLog: []entity.Change{
+				{
+					ID:              "12345",
+					Title:           "title 1",
+					SummaryMarkdown: &summaryMarkdown1,
+				},
+				{
+					ID:              "54321",
+					Title:           "title 2",
+					SummaryMarkdown: &summaryMarkdown2,
+				},
+			},
+			change: entity.Change{
+				Title:           "title 3",
+				SummaryMarkdown: &summaryMarkdown3,
+			},
+			expectedChange: entity.Change{
+				ID:              "test",
+				Title:           "title 3",
+				SummaryMarkdown: &summaryMarkdown3,
+				ReleasedAt:      &now,
+			},
+			availableKeys:         []service.Key{"test"},
+			expectedChangeLogSize: 3,
+			hasErr:                false,
+		}, {
+			name: "no available key",
+			changeLog: []entity.Change{
+				{
+					ID:              "12345",
+					Title:           "title 1",
+					SummaryMarkdown: &summaryMarkdown1,
+				},
+				{
+					ID:              "54321",
+					Title:           "title 2",
+					SummaryMarkdown: &summaryMarkdown2,
+				},
+			},
+			change: entity.Change{
+				Title:           "title 3",
+				SummaryMarkdown: &summaryMarkdown3,
+			},
+			expectedChange:        entity.Change{},
+			availableKeys:         []service.Key{},
+			expectedChangeLogSize: 2,
+			hasErr:                true,
+		}, {
+			name: "ID already exists",
+			changeLog: []entity.Change{
+				{
+					ID:              "12345",
+					Title:           "title 1",
+					SummaryMarkdown: &summaryMarkdown1,
+				},
+				{
+					ID:              "54321",
+					Title:           "title 2",
+					SummaryMarkdown: &summaryMarkdown2,
+				},
+			},
+			change: entity.Change{
+				Title:           "title 3",
+				SummaryMarkdown: &summaryMarkdown3,
+			},
+			expectedChange:        entity.Change{},
+			availableKeys:         []service.Key{"12345"},
+			expectedChangeLogSize: 2,
+			hasErr:                true,
+		}, {
+			name: "allow summary to be nil",
+			changeLog: []entity.Change{
+				{
+					ID:              "12345",
+					Title:           "title 1",
+					SummaryMarkdown: &summaryMarkdown1,
+				},
+				{
+					ID:              "54321",
+					Title:           "title 2",
+					SummaryMarkdown: &summaryMarkdown2,
+				},
+			},
+			change: entity.Change{
+				Title:           "title 3",
+				SummaryMarkdown: nil,
+			},
+			expectedChange: entity.Change{
+				ID:              "22222",
+				Title:           "title 3",
+				SummaryMarkdown: nil,
+				ReleasedAt:      &now,
+			},
+			availableKeys:         []service.Key{"22222"},
+			expectedChangeLogSize: 3,
+			hasErr:                false,
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			changeLogRepo := repository.NewChangeLogFake(testCase.changeLog)
+			keyFetcher := service.NewKeyFetcherFake(testCase.availableKeys)
+			keyGen, err := keygen.NewKeyGenerator(2, &keyFetcher)
+			mdtest.Equal(t, nil, err)
+
+			fakeTimer := mdtest.NewTimerFake(now)
+			persist := NewPersist(
+				keyGen,
+				fakeTimer,
+				&changeLogRepo,
+			)
+
+			newChange, err := persist.CreateChange(testCase.change.Title, testCase.change.SummaryMarkdown)
+			if testCase.hasErr {
+				mdtest.NotEqual(t, nil, err)
+				return
+			}
+			mdtest.Equal(t, nil, err)
+
+			mdtest.Equal(t, testCase.expectedChange, newChange)
+
+			changeLog, err := persist.GetChangeLog()
+			mdtest.Equal(t, nil, err)
+
+			mdtest.Equal(t, testCase.expectedChangeLogSize, len(changeLog))
+		})
+	}
+}
+
+func TestPersist_GetChangeLog(t *testing.T) {
+	t.Parallel()
+
+	now := time.Now()
+	summaryMarkdown1 := "summary 1"
+	summaryMarkdown2 := "summary 2"
+	testCases := []struct {
+		name          string
+		changeLog     []entity.Change
+		availableKeys []service.Key
+	}{
+		{
+			name: "get full changelog successfully",
+			changeLog: []entity.Change{
+				{
+					ID:              "12345",
+					Title:           "title 1",
+					SummaryMarkdown: &summaryMarkdown1,
+				},
+				{
+					ID:              "54321",
+					Title:           "title 2",
+					SummaryMarkdown: &summaryMarkdown2,
+				},
+			},
+			availableKeys: []service.Key{},
+		}, {
+			name:          "get empty changelog successfully",
+			changeLog:     []entity.Change{},
+			availableKeys: []service.Key{},
+		},
+	}
+
+	for _, testCase := range testCases {
+		testCase := testCase
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			changeLogRepo := repository.NewChangeLogFake(testCase.changeLog)
+			keyFetcher := service.NewKeyFetcherFake(testCase.availableKeys)
+			keyGen, err := keygen.NewKeyGenerator(2, &keyFetcher)
+			mdtest.Equal(t, nil, err)
+
+			fakeTimer := mdtest.NewTimerFake(now)
+			persist := NewPersist(
+				keyGen,
+				fakeTimer,
+				&changeLogRepo,
+			)
+
+			changeLog, err := persist.GetChangeLog()
+			mdtest.Equal(t, nil, err)
+
+			mdtest.SameElements(t, testCase.changeLog, changeLog)
+		})
+	}
+}
