e3741297436e257ea6a2a29250fb7d19bbcf5b7f
Harry Liu <byliuyang11@gmail.com>
2020-04-07 22:38:02 -0700

    Add meaningful error message for invalid recaptcha site key (#637)
    * Add meaningful error message for invalid recaptcha site key
    
    * Apply suggestions from code review

--- frontend/src/service/Error.service.ts
+++ frontend/src/service/Error.service.ts
@@ -1,6 +1,8 @@
 import { IErr } from '../entity/Err';
 
 export enum Err {
+  ReCaptchaNotReady = 'reCaptchaNotReady',
+  InvalidReCaptchaSiteKey = 'invalidReCaptchaSiteKey',
   AliasAlreadyExist = 'aliasAlreadyExist',
   UserNotHuman = 'requesterNotHuman',
   Unauthorized = 'invalidAuthToken',
@@ -15,6 +17,13 @@ const unknownErr = {
                 Please email byliuyang11@gmail.com the screenshots and detailed 
                 steps to reproduce it so that I can investigate.
                 `
+};
+
+const invalidReCaptchaSiteKeyErr = {
+  name: 'Invalid reCaptcha site key',
+  description: `
+  Please email byliuyang11@gmail.com the screenshots and detailed steps to 
+  reproduce it so that I can investigate.`
 };
 
 const aliasNotAvailableErr = {
@@ -51,6 +60,8 @@ export class ErrorService {
         return userNotHumanErr;
       case Err.NetworkError:
         return networkErr;
+      case Err.InvalidReCaptchaSiteKey:
+        return invalidReCaptchaSiteKeyErr;
       case Err.Unknown:
       default:
         return unknownErr;
