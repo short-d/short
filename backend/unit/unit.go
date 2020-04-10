b6cfed2528cdaf59612109d55b99574e8f250c39
Arber Avdullahu <arber_919@live.com>
2020-04-10 18:20:21 +0200

    refactor

--- backend/unit/unit.go
+++ backend/unit/unit.go
@@ -19,21 +19,17 @@ func ParseDuration(s string) (time.Duration, error) {
 		length   int
 	)
 	timeVariable := s[len(s)-1]
+	length, err = strconv.Atoi(s[:len(s)-1])
 	switch timeVariable {
 	case 's':
-		length, err = strconv.Atoi(s[:len(s)-1])
 		duration = time.Duration(length) * time.Second
 	case 'm':
-		length, err = strconv.Atoi(s[:len(s)-1])
 		duration = time.Duration(length) * time.Minute
 	case 'h':
-		length, err = strconv.Atoi(s[:len(s)-1])
 		duration = time.Duration(length) * time.Hour
 	case 'd':
-		length, err = strconv.Atoi(s[:len(s)-1])
 		duration = time.Duration(length) * oneDay
 	case 'w':
-		length, err = strconv.Atoi(s[:len(s)-1])
 		duration = time.Duration(length) * oneWeak
 	default:
 		err = errors.New("unknown time type")
