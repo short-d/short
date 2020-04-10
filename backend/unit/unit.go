052779cc9ed7e5bdcda8a573ec69ac0e12787bbc
Arber Avdullahu <arber_919@live.com>
2020-04-10 17:37:20 +0200

    fit too much returns

--- backend/unit/unit.go
+++ backend/unit/unit.go
@@ -13,24 +13,30 @@ const (
 
 // ParseDuration reads a human-readable duration and returns duration in seconds
 func ParseDuration(s string) (time.Duration, error) {
+	var (
+		duration time.Duration
+		err      error
+		length   int
+	)
 	timeVariable := s[len(s)-1]
 	switch timeVariable {
 	case 's':
-		length, err := strconv.Atoi(s[:len(s)-1])
-		return time.Duration(length) * time.Second, err
+		length, err = strconv.Atoi(s[:len(s)-1])
+		duration = time.Duration(length) * time.Second
 	case 'm':
-		length, err := strconv.Atoi(s[:len(s)-1])
-		return time.Duration(length) * time.Minute, err
+		length, err = strconv.Atoi(s[:len(s)-1])
+		duration = time.Duration(length) * time.Minute
 	case 'h':
-		length, err := strconv.Atoi(s[:len(s)-1])
-		return time.Duration(length) * time.Hour, err
+		length, err = strconv.Atoi(s[:len(s)-1])
+		duration = time.Duration(length) * time.Hour
 	case 'd':
-		length, err := strconv.Atoi(s[:len(s)-1])
-		return time.Duration(length) * oneDay, err
+		length, err = strconv.Atoi(s[:len(s)-1])
+		duration = time.Duration(length) * oneDay
 	case 'w':
-		length, err := strconv.Atoi(s[:len(s)-1])
-		return time.Duration(length) * oneWeak, err
+		length, err = strconv.Atoi(s[:len(s)-1])
+		duration = time.Duration(length) * oneWeak
 	default:
-		return 0, errors.New("unknown time type")
+		err = errors.New("unknown time type")
 	}
+	return duration, err
 }
