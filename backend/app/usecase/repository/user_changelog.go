abaad06818fdd8d7814399087a163e245b493ddc
Kapil Lamba <kapillamba4@gmail.com>
2020-04-13 13:00:33 +0530

    Add UserChangeLog respository (#656)
    * Add UserChangeLog respository
    
    * Make code review changes
    
    * Rename NewUserChangeLogSql to NewUserChangeLogSQL
    
    * Make code review changes
    
    * Make code review changes
    
    * Make code review changes
    
    * Make code review changes
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Add CreateLastViewedAt to repository
    
    * Make code review changes
    
    * Make code review changes
    
    * Apply suggestions from code review
    
    * Make code review changes
    
    * Make code review changes
    
    * Apply suggestions from code review
    
    Co-Authored-By: Harry Liu <byliuyang11@gmail.com>
    
    * Restart build
    
    * Run build again
    
    * Apply suggestions from code review
    
    * Make code review changes
    
    * Update user_changelog.go
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

---
+++ backend/app/usecase/repository/user_changelog.go
@@ -0,0 +1,14 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/short-d/short/app/entity"
+)
+
+// UserChangeLog accesses user-changelog information from storage, such as database.
+type UserChangeLog interface {
+	GetLastViewedAt(user entity.User) (time.Time, error)
+	UpdateLastViewedAt(user entity.User, currentTime time.Time) (time.Time, error)
+	CreateRelation(user entity.User, currentTime time.Time) error
+}
