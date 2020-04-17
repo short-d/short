8b08c1afe6c65ed38f787d4f9b74982d00b66c30
Harry Liu <byliuyang11@gmail.com>
2020-04-16 23:41:34 -0700

    Track interaction and coarse of the anonymous user

--- backend/app/adapter/instrumentation/instrumentation.go
+++ backend/app/adapter/instrumentation/instrumentation.go
@@ -2,6 +2,7 @@ package instrumentation
 
 import (
 	"github.com/short-d/app/fw"
+	"github.com/short-d/short/app/entity"
 	"github.com/short-d/short/app/usecase/observability"
 )
 
@@ -9,20 +10,39 @@ var _ observability.Observability = (*Instrumentation)(nil)
 
 // Instrumentation specifics how the system should be observed.
 type Instrumentation struct {
-	logger  fw.Logger
-	tracer  fw.Tracer
-	timer   fw.Timer
-	metrics fw.Metrics
-	ctx     fw.ExecutionContext
+	logger      fw.Logger
+	tracer      fw.Tracer
+	timer       fw.Timer
+	metrics     fw.Metrics
+	analytics   fw.Analytics
+	geoLocation fw.GeoLocation
+	ctx         fw.ExecutionContext
+}
+
+func (i Instrumentation) RedirectingAliasToLongLink(user *entity.User) {
+	userID := i.getUserID(user)
+	i.analytics.Track("RedirectingAliasToLongLink", map[string]string{}, userID, i.ctx)
+}
+
+func (i Instrumentation) RedirectedAliasToLongLink(user *entity.User) {
+	userID := i.getUserID(user)
+	i.analytics.Track("RedirectedAliasToLongLink", map[string]string{}, userID, i.ctx)
+}
+
+func (i Instrumentation) getUserID(user *entity.User) string {
+	if user == nil {
+		return i.ctx.RequestID
+	}
+	return user.Email
+}
+
+// LongLinkRetrievalSucceed tracks the successes when retrieving long links.
+func (i Instrumentation) LongLinkRetrievalSucceed() {
+	i.metrics.Count("long-link-retrieval-succeed", 1, 1, i.ctx)
 }
 
 // LongLinkRetrievalFailed tracks the failures when retrieving long links.
 func (i Instrumentation) LongLinkRetrievalFailed(err error) {
 	i.logger.Error(err)
 	i.metrics.Count("long-link-retrieval-failed", 1, 1, i.ctx)
-}
-
-// LongLinkRetrievalSucceed tracks the successes when retrieving long links.
-func (i Instrumentation) LongLinkRetrievalSucceed() {
-	i.metrics.Count("long-link-retrieval-succeed", 1, 1, i.ctx)
 }
