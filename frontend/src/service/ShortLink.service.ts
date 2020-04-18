c0e2582e97b3a99ffe7965c90f4a35092cf093f6
rohithbalaji123 <rohithbalaji123@gmail.com>
2020-04-18 22:17:32 +0530

    Add short link service with getting user created short links

--- frontend/src/service/ShortLink.service.ts
+++ frontend/src/service/ShortLink.service.ts
@@ -1,6 +1,38 @@
 import { Url } from '../entity/Url';
+import { Err, ErrorService } from './Error.service';
+import { AuthService } from './Auth.service';
+import { ApiService } from './Api.service';
 
 export interface IPagedShortLinks {
   shortLinks: Url[];
   totalCount: number;
+}
+
+export class ShortLinkService {
+  constructor(
+    private apiService: ApiService,
+    private authService: AuthService,
+    private errorService: ErrorService
+  ) {}
+
+  getUserCreatedShortLinks(
+    offset: number,
+    pageSize: number
+  ): Promise<IPagedShortLinks> {
+    return new Promise((resolve, reject) => {
+      this.apiService
+        .invokeGetUserShortLinksApi(offset, pageSize)
+        .then(pagedShortLinks => resolve(pagedShortLinks))
+        .catch(errCode => {
+          if (errCode === Err.Unauthenticated) {
+            reject({ authenticationErr: 'User is not authenticated' });
+            return;
+          }
+          reject({
+            getUserShortLinksErr: this.errorService.getErr(errCode)
+          });
+        });
+      return;
+    });
+  }
 }
