7cc6b78e90802007715973ceac3fbb91782ada6d
Rohith Balaji <rohithbalaji123@gmail.com>
2020-04-23 12:39:17 +0530

    Add ShortLink Sevice (#674)
    * Add Api service with get user shortlinks api
    
    * Add short link service with getting user created short links
    
    * Use custom graphQL service to fetch user short links data
    
    * Rename graphql URL interface to match server graphQL schema
    
    * Remove debugging console logs
    
    * Remove APIService interface and address review comments
    
    * Rename ApiService to ShortGraphQLApiService
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

--- frontend/src/service/ShortLink.service.ts
+++ frontend/src/service/ShortLink.service.ts
@@ -1,6 +1,51 @@
 import { Url } from '../entity/Url';
+import { Err, ErrorService } from './Error.service';
+import { AuthService } from './Auth.service';
+import { ShortGraphQLApiService } from './ShortGraphQLApiService';
 
 export interface IPagedShortLinks {
   shortLinks: Url[];
   totalCount: number;
+}
+
+export class ShortLinkService {
+  constructor(
+    private shortGraphQLApiService: ShortGraphQLApiService,
+    private authService: AuthService,
+    private errorService: ErrorService
+  ) {}
+
+  getUserCreatedShortLinks(
+    offset: number,
+    pageSize: number
+  ): Promise<IPagedShortLinks> {
+    return new Promise((resolve, reject) => {
+      // TODO(issue#673): support pagination for user created Short Links in API.
+      this.shortGraphQLApiService
+        .getUserShortLinks(offset, pageSize)
+        .then((URLs: Url[]) => {
+          resolve(this.getPagedShortLinks(URLs, offset, pageSize));
+        })
+        .catch((errCode: Err) => {
+          if (errCode === Err.Unauthenticated) {
+            reject({ authenticationErr: 'User is not authenticated' });
+            return;
+          }
+          reject({
+            getUserShortLinksErr: this.errorService.getErr(errCode)
+          });
+        });
+    });
+  }
+
+  private getPagedShortLinks(
+    urls: Url[],
+    offset: number,
+    pageSize: number
+  ): IPagedShortLinks {
+    return {
+      shortLinks: urls.slice(offset, offset + pageSize),
+      totalCount: urls.length
+    };
+  }
 }
