77b38c958f0b69ed75df4be63b154392f10da20f
Harry Liu <byliuyang11@gmail.com>
2020-04-23 20:09:18 -0700

    Cache feature toggle response in the memory  (#690)
    * Add in-memory cache
    
    * Cache feature decision

--- frontend/src/dep.ts
+++ frontend/src/dep.ts
@@ -18,6 +18,7 @@ import { GraphQLService } from './service/GraphQL.service';
 import { FetchHTTPService } from './service/HTTP.service';
 import { ShortHTTPApi } from './service/ShortHTTP.api';
 import { DynamicDecisionService } from './service/feature-decision/DynamicDecision.service';
+import { InMemoryCache } from './service/cache/in-memory.cache';
 
 export function initEnvService(): EnvService {
   return new EnvService();
@@ -43,7 +44,11 @@ export function initUIFactory(
   const errorService = new ErrorService();
   const httpService = new FetchHTTPService();
   const shortHTTPApi = new ShortHTTPApi(httpService, envService);
-  const dynamicDecisionService = new DynamicDecisionService(shortHTTPApi);
+  const inMemoryCache = new InMemoryCache();
+  const dynamicDecisionService = new DynamicDecisionService(
+    shortHTTPApi,
+    inMemoryCache
+  );
 
   const graphQLService = new GraphQLService(httpService);
   const urlService = new UrlService(
