49c1d26711c6db960aff5001a9d69f643cdb2842
Harry Liu <byliuyang11@gmail.com>
2020-04-27 17:52:18 -0700

    Format code

--- backend/env/env.go
+++ backend/env/env.go
@@ -4,11 +4,11 @@ import "github.com/short-d/app/fw"
 
 const (
 	// Production implies the server is running under production environment.
-	Production  fw.ServerEnv = "production"
+	Production fw.ServerEnv = "production"
 	// Staging implies the server is running under staging environment.
-	Staging     fw.ServerEnv = "staging"
+	Staging fw.ServerEnv = "staging"
 	// Development implies the server is running on developers local machine.
 	Development fw.ServerEnv = "development"
 	// Testing implies the server is running under testing environment.
-	Testing     fw.ServerEnv = "testing"
+	Testing fw.ServerEnv = "testing"
 )
