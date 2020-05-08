223ff37c366a75b1623765d68414f712b8f49a61
kapillamba4 <kapillamba4@gmail.com>
2020-05-08 10:45:56 +0530

    Add comments

--- backend/app/usecase/repository/user_changelog_fake.go
+++ backend/app/usecase/repository/user_changelog_fake.go
@@ -9,11 +9,12 @@ import (
 
 var _ UserChangeLog = (*UserChangeLogFake)(nil)
 
-// UserFake represents in memory implementation of user repository.
+// UserChangeLogFake represents in memory implementation of UserChangeLog repository.
 type UserChangeLogFake struct {
 	lastViewedAt map[string]time.Time
 }
 
+// GetLastViewedAt retrieves lastViewedAt for user
 func (u UserChangeLogFake) GetLastViewedAt(user entity.User) (time.Time, error) {
 	if lastViewedAt, exists := u.lastViewedAt[user.Email]; exists {
 		return lastViewedAt.UTC(), nil
@@ -22,6 +23,7 @@ func (u UserChangeLogFake) GetLastViewedAt(user entity.User) (time.Time, error) 
 	return time.Time{}, ErrEntryNotFound("user not found")
 }
 
+// UpdateLastViewedAt updates lastViewedAt for user to currentTime
 func (u *UserChangeLogFake) UpdateLastViewedAt(user entity.User, currentTime time.Time) (time.Time, error) {
 	if _, exists := u.lastViewedAt[user.Email]; exists {
 		u.lastViewedAt[user.Email] = currentTime.UTC()
@@ -31,6 +33,7 @@ func (u *UserChangeLogFake) UpdateLastViewedAt(user entity.User, currentTime tim
 	return time.Time{}, ErrEntryNotFound("user not found")
 }
 
+// CreateRelation inserts new entry into UserChangeLog repository
 func (u *UserChangeLogFake) CreateRelation(user entity.User, currentTime time.Time) error {
 	if _, exists := u.lastViewedAt[user.Email]; exists {
 		return errors.New("user exists")
@@ -40,6 +43,7 @@ func (u *UserChangeLogFake) CreateRelation(user entity.User, currentTime time.Ti
 	return nil
 }
 
+// NewUserChangeLogFake creates in memory UserChangeLog repository
 func NewUserChangeLogFake(lastViewedAt map[string]time.Time) UserChangeLogFake {
 	return UserChangeLogFake{
 		lastViewedAt: lastViewedAt,
