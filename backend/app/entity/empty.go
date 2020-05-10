c7e6d71cc6a07dc2cd3e5843dea0bdb6110e9535
Harry Liu <byliuyang11@gmail.com>
2020-05-10 10:37:00 -0700

    Add Authorizer to manager user roles and permission (#757)
    * Add prototype
    
    * Give a Role entity to handle permission logic
    
    * Add tests + slightly cleanup code
    
    * UserRoleSQL integration tests
    
    * One-to-many design implementation
    
    * Remove redundant check
    
    * Code review fixes
    
    * Code review fixes
    
    * Fix codeclimate issues
    
    * Clean up code
    
    * Add unit test for role-based access control
    
    * Add comment
    
    * Format code
    
    Co-authored-by: Alexander Petrov <alldroll@gmail.com>

---
+++ backend/app/entity/empty.go
@@ -0,0 +1,5 @@
+package entity
+
+// Empty is a placeholder for an empty struct to reduce memory usage when
+// initializing data structures, such as map with non empty key but empty value.
+type Empty struct{}
