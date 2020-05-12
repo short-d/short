e013a9e800bee7b512c876fb5472d3406e81fefa
Harry Liu <byliuyang11@gmail.com>
2020-05-11 22:43:08 -0700

    Create Facebook & Google SSO table

---
+++ backend/app/adapter/sqldb/migration/20_create_facebook_sso_table.sql
@@ -0,0 +1,10 @@
+-- +migrate Up
+CREATE TABLE facebook_sso
+(
+    facebook_user_id CHARACTER VARYING(254) NOT NULL UNIQUE,
+    short_user_id  CHARACTER VARYING(5) NOT NULL UNIQUE,
+    FOREIGN KEY (short_user_id) REFERENCES "user"(id) ON UPDATE CASCADE
+);
+
+-- +migrate Down
+DROP TABLE facebook_sso;
