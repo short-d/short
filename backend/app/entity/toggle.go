dcdd26af4629ba315b6322152367f1bfd7b9d208
rohithbalaji123 <rohithbalaji123@gmail.com>
2020-05-21 11:28:38 +0530

    Add type column to feature toggle table

--- backend/app/entity/toggle.go
+++ backend/app/entity/toggle.go
@@ -1,7 +1,14 @@
 package entity
 
+import "database/sql"
+
+type ToggleType = sql.NullString
+
+var PermissionToggle = ToggleType{String: "permission", Valid: true}
+
 // Toggle represents a controllable switch that can be turned on or off.
 type Toggle struct {
 	ID        string
 	IsEnabled bool
+	Type      ToggleType
 }
