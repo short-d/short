8f1691eaf8ef32d9f7a2f6672c74cd25e2c67176
Arber Avdullahu <arber_919@live.com>
2020-05-23 09:33:57 +0200

    rename usecase/repository (#806)
    * rename usecase/repository
    
    * review
    
    * review
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

--- backend/app/usecase/repository/userurl.go
+++
@@ -1,9 +0,0 @@
-package repository
-
-import "github.com/short-d/short/backend/app/entity"
-
-// UserURLRelation accesses User-ShortLink relationship from storage, such as database.
-type UserURLRelation interface {
-	CreateRelation(user entity.User, url entity.ShortLink) error
-	FindAliasesByUser(user entity.User) ([]string, error)
-}
