2ae2bbe85f4664fbbeb386701188a5a3fd472acf
Arber Avdullahu <arber_919@live.com>
2020-06-03 20:37:21 +0200

    search resources

---
+++ backend/app/usecase/search/order/unchanged.go
@@ -0,0 +1,18 @@
+package order
+
+import "github.com/short-d/short/backend/app/entity"
+
+var _ Order = (*Unchanged)(nil)
+
+// Unchanged represents the Order with unchanged arrangement of searchable resources.
+type Unchanged struct{}
+
+// ArrangeShortLinks keeps the same arrangement of shortLinks.
+func (u Unchanged) ArrangeShortLinks(shortLinks []entity.ShortLink) []entity.ShortLink {
+	return shortLinks
+}
+
+// ArrangeShortLinks keeps the same arrangement of users.
+func (u Unchanged) ArrangeUsers(users []entity.User) []entity.User {
+	return users
+}
