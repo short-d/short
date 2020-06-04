cd51afa0c7486a34c71b45f378bfbb75ce8b945e
Kapil Lamba <kapillamba4@gmail.com>
2020-06-04 19:56:00 +0530

    Add comments

--- backend/app/entity/metatag/metatag.go
+++ backend/app/entity/metatag/metatag.go
@@ -1,11 +1,13 @@
 package metatag
 
+// OpenGraph represents OpenGraph Meta Tags for a ShortLink.
 type OpenGraph struct {
 	Title       *string
 	Description *string
 	ImageURL    *string
 }
 
+// Twitter represents Twitter Meta Tags for a ShortLink.
 type Twitter struct {
 	Title       *string
 	Description *string
