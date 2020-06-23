b362cb37dd39686ee9b716e5ba315d52a66c8a14
Arber Avdullahu <arber_919@live.com>
2020-06-23 19:37:18 +0200

    unmarshal search request

--- backend/app/adapter/routing/handle/search.go
+++ backend/app/adapter/routing/handle/search.go
@@ -6,12 +6,19 @@ import (
 	"net/http"
 
 	"github.com/short-d/app/fw/router"
+	"github.com/short-d/short/backend/app/entity"
 	"github.com/short-d/short/backend/app/usecase/authenticator"
 	"github.com/short-d/short/backend/app/usecase/search"
 	"github.com/short-d/short/backend/app/usecase/search/order"
 )
 
-// Search fetches resources under certain criterias.
+// SearchRequest represents the JSON request received from Search API.
+type SearchRequest struct {
+	query  search.Query
+	filter search.Filter
+}
+
+// Search fetches resources under certain criteria.
 func Search(
 	searcher search.Search,
 	authenticator authenticator.Authenticator,
@@ -23,40 +30,59 @@ func Search(
 			return
 		}
 
-		searchRequest, err := readSearchRequest(r)
+		searchBody, err := parseSearchBody(r)
 		if err != nil {
 			http.Error(w, err.Error(), 500)
 			return
 		}
-
-		_ = search.Query{
-			Query: searchRequest.Query,
-			User:  user,
-		}
-		_, err = getFilter(searchRequest)
-		if err != nil {
-			http.Error(w, err.Error(), 500)
-			return
-		}
+		searchBody.setUser(user)
 
 		w.Write([]byte("request is read"))
 	}
 }
 
-// SearchRequest represents the JSON request received from Search API.
-type SearchRequest struct {
-	Query  string `json:"query"`
-	Filter Filter `json:"filter"`
+func (s *SearchRequest) UnmarshalJSON(data []byte) error {
+	temp := struct {
+		Query  string `json:"query"`
+		Filter struct {
+			MaxResults int      `json:"max_results"`
+			Resources  []string `json:"resources"`
+			Orders     []string `json:"orders"`
+		} `json:"filter"`
+	}{}
+
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+
+	s.setQuery(temp.Query)
+
+	resources := parseResources(temp.Filter.Resources)
+	orders := parseOrders(temp.Filter.Orders)
+	err := s.setFilter(temp.Filter.MaxResults, resources, orders)
+
+	return err
 }
 
-// Filter represents the filter JSON field received from Search API.
-type Filter struct {
-	MaxResults int      `json:"max_results"`
-	Resources  []string `json:"resources"`
-	Orders     []string `json:"orders"`
+func (s *SearchRequest) setQuery(query string) {
+	s.query.Query = query
 }
 
-func readSearchRequest(r *http.Request) (*SearchRequest, error) {
+func (s *SearchRequest) setFilter(maxResults int, resources []search.Resource, orders []order.By) error {
+	filter, err := search.NewFilter(maxResults, resources, orders)
+	if err != nil {
+		return err
+	}
+
+	s.filter = filter
+	return nil
+}
+
+func (s *SearchRequest) setUser(user *entity.User) {
+	s.query.User = user
+}
+
+func parseSearchBody(r *http.Request) (*SearchRequest, error) {
 	b, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -68,15 +94,9 @@ func readSearchRequest(r *http.Request) (*SearchRequest, error) {
 	return &searchRequest, err
 }
 
-func getFilter(request *SearchRequest) (search.Filter, error) {
-	resources := getResources(request)
-	orders := getOrders(request)
-	return search.NewFilter(request.Filter.MaxResults, resources, orders)
-}
-
-func getResources(request *SearchRequest) []search.Resource {
+func parseResources(data []string) []search.Resource {
 	var resources []search.Resource
-	for _, resource := range request.Filter.Resources {
+	for _, resource := range data {
 		if resource == "short_link" {
 			resources = append(resources, search.ShortLink)
 		} else if resource == "user" {
@@ -86,9 +106,9 @@ func getResources(request *SearchRequest) []search.Resource {
 	return resources
 }
 
-func getOrders(request *SearchRequest) []order.By {
+func parseOrders(data []string) []order.By {
 	var orders []order.By
-	for _, o := range request.Filter.Orders {
+	for _, o := range data {
 		if o == "by_created_time_asc" {
 			orders = append(orders, order.ByCreatedTimeASC)
 		}
