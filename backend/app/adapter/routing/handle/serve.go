f23807ba5e221cd2f449efda6869ba282443a623
Harry Liu <byliuyang11@gmail.com>
2020-06-26 13:23:26 -0700

    Add comments

--- backend/app/adapter/routing/handle/serve.go
+++ backend/app/adapter/routing/handle/serve.go
@@ -6,6 +6,7 @@ import (
 	"github.com/short-d/app/fw/router"
 )
 
+// ServeDir reads files in the given directory and makes them accessible on the web.
 func ServeDir(dir string) router.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params router.Params) {
 		fs := http.FileServer(http.Dir(dir))
@@ -13,6 +14,7 @@ func ServeDir(dir string) router.Handle {
 	}
 }
 
+// ServeDir reads a given file and makes it accessible on the web.
 func ServeFile(fileName string) router.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params router.Params) {
 		http.ServeFile(w, r, fileName)
