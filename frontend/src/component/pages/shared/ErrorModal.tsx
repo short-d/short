15985f29f600bf8094c7cf0eb1315f4ce4035029
rohithbalaji123 <rohithbalaji123@gmail.com>
2020-06-26 13:05:33 +0530

    Extract error modal into a separate component

---
+++ frontend/src/component/pages/shared/ErrorModal.tsx
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import './ErrorModal.scss';
+
+import { IErr } from '../../../entity/Err';
+import { Icon, IconID } from '../../ui/Icon';
+import { Modal } from '../../ui/Modal';
+import { Store, Unsubscribe } from 'redux';
+import { IAppState } from '../../../state/reducers';
+import { clearError } from '../../../state/actions';
+
+interface IProps {
+  store: Store<IAppState>;
+}
+
+interface IState {
+  error?: IErr;
+}
+
+export class ErrorModal extends Component<IProps, IState> {
+  private modalRef = React.createRef<Modal>();
+  private stopErrorListener: Unsubscribe | undefined;
+
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      error: props.store.getState().err
+    };
+  }
+
+  componentDidMount(): void {
+    this.startErrorListener();
+  }
+
+  private startErrorListener() {
+    this.stopErrorListener = this.props.store.subscribe(() => {
+      const { err } = this.props.store.getState();
+      this.setState({ error: err });
+      if (err) {
+        this.showError();
+      }
+    });
+  }
+
+  componentWillUnmount(): void {
+    if (this.stopErrorListener) {
+      this.stopErrorListener();
+    }
+  }
+
+  render() {
+    const { error } = this.state;
+
+    return (
+      <Modal
+        canClose={true}
+        onModalClose={this.handleModalClose}
+        ref={this.modalRef}
+      >
+        <div className={'error-modal-content'}>
+          <div className={'close-icon'}>
+            <Icon
+              defaultIconID={IconID.Close}
+              onClick={this.handleCloseClick}
+            />
+          </div>
+          <div className={'title'}>{error?.name}</div>
+          <div className={'description'}>{error?.description}</div>
+        </div>
+      </Modal>
+    );
+  }
+
+  private handleModalClose = () => {
+    this.props.store.dispatch(clearError());
+  };
+
+  private handleCloseClick = () => {
+    this.modalRef.current!.close();
+  };
+
+  private showError = () => {
+    this.modalRef.current!.open();
+  };
+}
