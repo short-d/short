28c9ed6658f8f5fb9f9f04fc14c29c21f6aa2dd7
Harry Liu <byliuyang11@gmail.com>
2020-06-26 00:45:31 -0700

    Remove width & height

--- frontend/src/component/ui/Icon.tsx
+++ frontend/src/component/ui/Icon.tsx
@@ -122,9 +122,7 @@ export class Icon extends Component<IProps, IStates> {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        height="24"
         viewBox="0 0 24 24"
-        width="24"
       >
         <path d="M0 0h24v24H0z" fill="none" />
         <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
@@ -136,9 +134,7 @@ export class Icon extends Component<IProps, IStates> {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        height="24"
         viewBox="0 0 24 24"
-        width="24"
       >
         <path d="M0 0h24v24H0z" fill="none" />
         <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
