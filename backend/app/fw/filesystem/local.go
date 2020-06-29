aad5d9e2b38b74af71d2dc2e6d548ae956468a32
Harry Liu <byliuyang11@gmail.com>
2020-06-29 09:40:48 -0700

    Add comments

--- backend/app/fw/filesystem/local.go
+++ backend/app/fw/filesystem/local.go
@@ -4,13 +4,16 @@ import "io/ioutil"
 
 var _ FileSystem = (*Local)(nil)
 
+// Local saves and reads files from local disk.
 type Local struct {
 }
 
+// ReadFile reads the content of the given file from local disk.
 func (l Local) ReadFile(filepath string) ([]byte, error) {
 	return ioutil.ReadFile(filepath)
 }
 
+// NewLocal creates Local file system.
 func NewLocal() Local {
 	return Local{}
 }
