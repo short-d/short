d816af021f4f67300aad9047fd9bfc0458914412
Harry Liu <byliuyang11@gmail.com>
2020-06-28 18:56:52 -0700

    Add example usage

--- widget/emotic/example/src/App.tsx
+++ widget/emotic/example/src/App.tsx
@@ -1,10 +1,88 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-import { ExampleComponent } from 'emotic'
+import { Emotic, EmotionType, Feedback } from 'emotic'
 import 'emotic/dist/index.css'
 
-const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+import './App.scss'
+
+interface IState {
+  showDiscount: boolean
+  showRefund: boolean
+}
+
+export class App extends Component<any, IState> {
+  constructor(props: any) {
+    super(props)
+
+    this.state = {
+      showDiscount: false,
+      showRefund: false
+    }
+  }
+
+  render() {
+    return (
+      <div className={'App'}>
+        {this.state.showDiscount && (
+          <div className={'message'}>
+            Hey, we saw you are not satisfied with our service.
+            <br />
+            We highly value you as our customer.
+            <br />
+            <br />
+            We want to keep you here. How about having your next month's
+            subscription for <a href={'/next-month-free'}>free</a>?
+            <br />
+            That's on us!
+          </div>
+        )}
+        {this.state.showRefund && (
+          <div className={'message'}>
+            We are very sorry to hear that you are frustrated with our service.
+            <br />
+            <br />
+            We will carefully review your feedback and let you know immediately
+            when the problem get fixed.
+            <br />
+            <br />
+            Your trust is our top priority.
+            <br />
+            <br />
+            In case you want to <a href={'/refund'}>cancel</a> the service, we
+            can provide you the full refund.
+          </div>
+        )}
+        <Emotic onFeedbackFiled={this.handleOnFeedbackFiled} />
+      </div>
+    )
+  }
+
+  handleOnFeedbackFiled = (feedback: Feedback) => {
+    this.setState({
+      showDiscount: false,
+      showRefund: false
+    })
+    switch (feedback.emotion) {
+      case EmotionType.Hate:
+        this.showDiscount()
+        break
+      case EmotionType.Terrible:
+        this.showRefund()
+        break
+    }
+  }
+
+  showDiscount = () => {
+    this.setState({
+      showDiscount: true
+    })
+  }
+
+  showRefund = () => {
+    this.setState({
+      showRefund: true
+    })
+  }
 }
 
 export default App
