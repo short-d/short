34eb673bfb4d5ac25335cbe171d2c91c0ef93a7a
Harry Liu <byliuyang11@gmail.com>
2020-07-01 10:12:13 -0700

    Apply suggestions from code review

--- backend/app/adapter/sqldb/app.go
+++ backend/app/adapter/sqldb/app.go
@@ -17,7 +17,7 @@ type AppSQL struct {
 	db *sql.DB
 }
 
-// FindAppByID  fetches an app with given ID from SQL database.
+// FindAppByID fetches an app with given ID from SQL DB.
 func (a AppSQL) FindAppByID(id string) (entity.App, error) {
 	query := fmt.Sprintf(`
 SELECT "%s", "%s" 
