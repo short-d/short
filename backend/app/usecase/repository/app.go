afe6defc6ce7500118631e87b429398ee6b0c54d
Harry Liu <byliuyang11@gmail.com>
2020-07-01 10:10:02 -0700

    Add comment

--- backend/app/usecase/repository/app.go
+++ backend/app/usecase/repository/app.go
@@ -2,6 +2,7 @@ package repository
 
 import "github.com/short-d/short/backend/app/entity"
 
+// App accesses third party app info from persistent storage, such as database.
 type App interface {
 	FindAppByID(appID string) (entity.App, error)
 }
