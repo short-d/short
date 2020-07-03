71c83b57f4d6ec6f23a26e57845a60752baea9bd
Harry Liu <byliuyang11@gmail.com>
2020-07-02 21:08:26 -0700

    Add ApiKey repository (#926)
    * Add ApiKey repository
    
    * Clean up code
    
    * Apply suggestions from code review
    
    * Update backend/app/adapter/sqldb/table/apikey.go

--- backend/app/usecase/repository/error.go
+++ backend/app/usecase/repository/error.go
@@ -6,6 +6,17 @@ var _ error = (*ErrEntryNotFound)(nil)
 // ErrEntryNotFound represents table entry not found error.
 type ErrEntryNotFound string
 
+// Error coverts ErrEntryNotFound into human readable message to easy debugging.
 func (e ErrEntryNotFound) Error() string {
+	return string(e)
+}
+
+var _ error = (*ErrEntryExists)(nil)
+
+// ErrEntryExists represents table entry already exists error.
+type ErrEntryExists string
+
+// Error coverts ErrEntryExists into human readable message to easy debugging.
+func (e ErrEntryExists) Error() string {
 	return string(e)
 }
