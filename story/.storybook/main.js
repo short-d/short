f57964d6ebdd23617f097460685027222e8f4c50
Harry Liu <byliuyang11@gmail.com>
2020-07-11 12:46:49 -0700

    Create story for button (#943)

--- story/.storybook/main.js
+++ story/.storybook/main.js
@@ -1,9 +1,13 @@
 module.exports = {
     stories: ['../src/**/*.stories.tsx'],
+    addons: [
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-actions/register'
+    ],
     webpackFinal: async config => {
         config.module.rules.push({
             test: /\.(ts|tsx)$/,
-            use: ['ts-loader'],
+            use: ['ts-loader', 'react-docgen-typescript-loader'],
         }, {
             test: /\.scss$/,
             use: [
