bfdc08aa532e576076a581684a50acab351994e2
rohithbalaji123 <rohithbalaji123@gmail.com>
2020-07-19 23:41:15 +0530

    Separate create modal into a separate component

---
+++ frontend/src/component/pages/admin/changelog/CreateChangeModal.tsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Modal } from '../../../ui/Modal';
+import styles from './CreateChangeModal.module.scss';
+import { Icon, IconID } from '../../../ui/Icon';
+import { CreateChangeSection } from './CreateChangeSection';
+import { Store } from 'redux';
+import { IAppState } from '../../../../state/reducers';
+import { ChangeLogService } from '../../../../service/ChangeLog.service';
+
+interface IProps {
+  store: Store<IAppState>;
+  changeLogService: ChangeLogService;
+  onAuthenticationFailed: () => void;
+  handleOnChangeCreated: () => void;
+}
+
+export class CreateChangeModal extends Component<IProps> {
+  private modalRef = React.createRef<Modal>();
+
+  render() {
+    return (
+      <Modal ref={this.modalRef} canClose={true}>
+        <div className={styles.modalCloseIcon}>
+          <Icon
+            defaultIconID={IconID.Close}
+            onClick={this.handleOnModalCloseClick}
+          />
+        </div>
+        <div className={styles.createChangeSectionContainer}>
+          <CreateChangeSection
+            onChangeCreated={this.props.handleOnChangeCreated}
+            changeLogService={this.props.changeLogService}
+            onAuthenticationFailed={this.props.onAuthenticationFailed}
+            store={this.props.store}
+          />
+        </div>
+      </Modal>
+    );
+  }
+
+  private handleOnModalCloseClick = () => {
+    this.close();
+  };
+
+  open = () => {
+    this.modalRef.current!.open();
+  };
+
+  close = () => {
+    this.modalRef.current!.close();
+  };
+}
