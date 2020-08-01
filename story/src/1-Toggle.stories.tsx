5c8d853f74697cfdcf7334b2415026b435145d90
Coteh <3276350+Coteh@users.noreply.github.com>
2020-07-31 21:07:01 -0400

    Add styles to Toggle + update storybook

--- story/src/1-Toggle.stories.tsx
+++ story/src/1-Toggle.stories.tsx
@@ -12,3 +12,43 @@ export default {
 export const pink = () => {
   return <Toggle defaultIsEnabled={false} onClick={action('click')}></Toggle>;
 };
+
+export const blue = () => {
+  return (
+    <Toggle
+      styles={['blue']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const black = () => {
+  return (
+    <Toggle
+      styles={['black']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const red = () => {
+  return (
+    <Toggle
+      styles={['red']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const shadow = () => {
+  return (
+    <Toggle
+      styles={['pink', 'shadow']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
