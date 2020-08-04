c2f83f1dda2ea12d126ce7bf1b501df460a7324c
James Cote <3276350+Coteh@users.noreply.github.com>
2020-08-04 00:42:06 -0400

    Add styles to Toggle (#988)
    * Add styles to Toggle + update storybook
    
    * Add Green style to Button and Toggle storybooks
    
    * Remove unrelated item from PR
    
    * Remove unneeded styles
    
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>

--- story/src/1-Toggle.stories.tsx
+++ story/src/1-Toggle.stories.tsx
@@ -12,3 +12,53 @@ export default {
 export const pink = () => {
   return <Toggle defaultIsEnabled={false} onClick={action('click')}></Toggle>;
 };
+
+export const blue = () => {
+  return (
+    <Toggle
+      styles={['blue']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const black = () => {
+  return (
+    <Toggle
+      styles={['black']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const red = () => {
+  return (
+    <Toggle
+      styles={['red']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const green = () => {
+  return (
+    <Toggle
+      styles={['green']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
+
+export const shadow = () => {
+  return (
+    <Toggle
+      styles={['pink', 'shadow']}
+      defaultIsEnabled={false}
+      onClick={action('click')}
+    ></Toggle>
+  );
+};
