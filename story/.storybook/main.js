c05cff4eba36719a6dd9212e1a39550fa7de2acb
Coteh <3276350+Coteh@users.noreply.github.com>
2020-08-18 03:30:28 -0400

    Remove TypeScript config, superseded by Storybook 6 default config

--- story/.storybook/main.js
+++ story/.storybook/main.js
@@ -1,26 +1,24 @@
 module.exports = {
-    stories: ['../src/**/*.stories.tsx'],
-    addons: [
-        '@storybook/addon-knobs/register',
-        '@storybook/addon-actions/register'
-    ],
-    webpackFinal: async config => {
-        config.module.rules.push({
-            test: /\.(ts|tsx)$/,
-            use: ['ts-loader', 'react-docgen-typescript-loader'],
-        }, {
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
-                    },
-                },
-                'sass-loader'],
-        });
-        config.resolve.extensions.push('.ts', '.tsx', '.scss');
-        return config;
-    },
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-actions/register'
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        },
+        'sass-loader'
+      ]
+    });
+    config.resolve.extensions.push('.ts', '.tsx', '.scss');
+    return config;
+  }
 };
