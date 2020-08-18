42d3e506f3c99368cc59439176ff65fe8b636ab6
Aaron Shakibpanah <aaronshakib@gmail.com>
2020-08-17 22:51:57 -0400

    Merge into prod: localhost api docs and delete-short-link feature (#997)
    * Add deletion feature to short link repository (#969)
    
    * Add `DeleteShortLink` to short link repository interface.
    * Add `ErrAliasNotFound` to short link repository errors.
    
    Co-authored-by: James Cote <3276350+Coteh@users.noreply.github.com>
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>
    
    * Add localhost for swagger docs (#994)
    
    * add localhost
    
    * add HTTP
    
    * remove `.com`
    
    Co-authored-by: Aaron Shakibpanah <ashakibpanah@hubspot.com>
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>
    
    Co-authored-by: Oscar Tovar <33381041+oatovar@users.noreply.github.com>
    Co-authored-by: James Cote <3276350+Coteh@users.noreply.github.com>
    Co-authored-by: Harry Liu <byliuyang11@gmail.com>
    Co-authored-by: Aaron Shakibpanah <ashakibpanah@hubspot.com>

--- backend/app/usecase/repository/shortlink.go
+++ backend/app/usecase/repository/shortlink.go
@@ -1,8 +1,22 @@
 package repository
 
 import (
+	"fmt"
+
 	"github.com/short-d/short/backend/app/entity"
 )
+
+var _ error = (*ErrAliasNotFound)(nil)
+
+// ErrAliasNotFound represents no short link entry found
+// with the given alias.
+type ErrAliasNotFound struct {
+	Alias string
+}
+
+func (e ErrAliasNotFound) Error() string {
+	return fmt.Sprintf("short link with alias(%s) not found", e.Alias)
+}
 
 // ShortLink accesses shortLinks from storage, such as database.
 type ShortLink interface {
@@ -10,5 +24,6 @@ type ShortLink interface {
 	GetShortLinkByAlias(alias string) (entity.ShortLink, error)
 	CreateShortLink(shortLinkInput entity.ShortLinkInput) error
 	UpdateShortLink(oldAlias string, shortLinkInput entity.ShortLinkInput) (entity.ShortLink, error)
+	DeleteShortLink(alias string) error
 	GetShortLinksByAliases(aliases []string) ([]entity.ShortLink, error)
 }
