b5755b116eb795fe6fe2e9522f452536fccbd293
Coteh <3276350+Coteh@users.noreply.github.com>
2020-08-20 02:54:43 -0400

    Add Storybook Controls to Button story

--- story/src/0-Button.stories.tsx
+++ story/src/0-Button.stories.tsx
@@ -1,63 +1,71 @@
 import React from 'react';
 import { Button } from '../../frontend/src/component/ui/Button';
 import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
 
 import styles from './0-Button.stories.module.scss';
 
 export default {
   title: 'UI/Button',
-  component: Button
+  component: Button,
+  argTypes: {
+    label: {
+      type: 'text',
+      description: 'Button text',
+      defaultValue: 'Button',
+      table: {
+        type: { summary: 'string' },
+        defaultValue: { summary: 'Button' }
+      },
+      control: {
+        type: 'text'
+      }
+    },
+    color: {
+      defaultValue: 'blue',
+      description: 'Button color',
+      control: {
+        type: 'select',
+        options: ['pink', 'blue', 'black', 'red', 'green']
+      }
+    },
+    shadow: {
+      defaultValue: false,
+      description: 'Button shadow',
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
 };
 
-export const pink = () => {
-  return <Button onClick={action('click')}>{text('Label', 'Button')}</Button>;
-};
-
-export const blue = () => {
+export const Primary = ({ label, color, shadow, ...args }) => {
+  let shadowStyle: string;
+  if (shadow) {
+    shadowStyle = 'shadow';
+  } else {
+    shadowStyle = '';
+  }
   return (
-    <Button styles={['blue']} onClick={action('click')}>
-      {text('Label', 'Button')}
+    <Button {...args} styles={[color, shadowStyle]} onClick={action('click')}>
+      {label}
     </Button>
   );
 };
 
-export const black = () => {
+export const FullWidth = ({ label, color, shadow, ...args }) => {
+  let shadowStyle: string;
+  if (shadow) {
+    shadowStyle = 'shadow';
+  } else {
+    shadowStyle = '';
+  }
   return (
-    <Button styles={['black']} onClick={action('click')}>
-      {text('Label', 'Button')}
-    </Button>
-  );
-};
-
-export const red = () => {
-  return (
-    <Button styles={['red']} onClick={action('click')}>
-      {text('Label', 'Button')}
-    </Button>
-  );
-};
-
-export const green = () => {
-  return (
-    <Button styles={['green']} onClick={action('click')}>
-      {text('Label', 'Button')}
-    </Button>
-  );
-};
-
-export const fullWidth = () => {
-  return (
-    <Button styles={['pink', 'full-width']} onClick={action('click')}>
-      <div className={styles.content}>{text('Label', 'Button')}</div>
-    </Button>
-  );
-};
-
-export const shadow = () => {
-  return (
-    <Button styles={['pink', 'shadow']} onClick={action('click')}>
-      {text('Label', 'Button')}
+    <Button
+      {...args}
+      styles={[color, shadowStyle, 'full-width']}
+      onClick={action('click')}
+    >
+      <div className={styles.content}>{label}</div>
     </Button>
   );
 };
