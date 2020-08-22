609e718e9891d3b85fd61c56ef17f3e4597b483f
Tlac <thomaslac912@gmail.com>
2020-08-22 03:18:16 -0400

    Move style to its own file

--- story/src/2-Icon.stories.tsx
+++ story/src/2-Icon.stories.tsx
@@ -2,70 +2,71 @@ import React from 'react';
 import { Icon, IconID } from '../../frontend/src/component/ui/Icon';
 import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
-
+import styles from './2-Icon.stories.module.scss'
 export default {
   title: 'UI/Icon',
   component: <Icon iconID={IconID.Menu} />,
   decorators: [withInfo({ header: false, inline: true })]
 }
 
-const iconStyle = {
-  width: '24px',
-  height: '24px'
-};
-
 export const menu = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Menu} onClick={action('click')}/>
     </div>
   )
 }
+
 export const menuOpen = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.MenuOpen} onClick={action('click')}/>
     </div>
   )
 }
+
 export const close = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Close} onClick={action('click')}/>
     </div>
   )
 }
+
 export const search = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Search} onClick={action('click')}/>
     </div>
   )
 }
+
 export const edit = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Edit} onClick={action('click')}/>
     </div>
   )
 }
 export const check = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Check} onClick={action('click')}/>
     </div>
   )
 }
+
 export const deleteIcon = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.Delete} onClick={action('click')}/>
     </div>
   )
 }
+
 export const rightArrow = () => {
   return (
-    <div style={iconStyle}>
+    <div className={styles.icon}>
       <Icon iconID={IconID.RightArrow} onClick={action('click')}/>
     </div>
   )
